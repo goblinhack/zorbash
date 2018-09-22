f89f6e403dfcc687889b58e2a5da5c99dd19250d
Neil McGill <goblinhack@gmail.com>
2018-09-22 15:30:31 -0400

    smooth lighting
    Former-commit-id: 2bf311d9faa34884c9f577baa40ba6fe8eaa3345

--- src/thing_map.cpp
+++ src/thing_map.cpp
@@ -493,37 +493,30 @@ void thing_render_all (void)
             fpoint at = game.state.player->at;
             at.x += 0.5;
             at.y += 0.5;
-            map_light_add(game.state.player->tp, at, 10.0, RED);
+            map_light_add(game.state.player->tp, at, 10.0, WHITE);
 
-//            at.x -= 5;
-//            at.y += 1;
-//            map_light_add(game.state.player->tp, at, 3.0, RED);
+            at.x -= 5;
+            at.y += 1;
+            map_light_add(game.state.player->tp, at, 3.0, RED);
 
-//            at.x += 3;
-//            at.y -= 3;
-//            map_light_add(game.state.player->tp, at, 3.0, GREEN);
+            at.x += 3;
+            at.y -= 3;
+            map_light_add(game.state.player->tp, at, 3.0, GREEN);
 
-blit_fbo_bind(FBO_WID);
             map_light_calculate_visible(0);
-//            map_light_display(0, FBO_VISITED_MAP, true);
+            map_light_display(0, FBO_VISITED_MAP_MERGED, true);
         }
     }
     
-#if 0
     blit_fbo_bind(FBO_WID);
-#endif
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     thing_map_blit_background();
     thing_blit_things(minx, miny, minz, maxx, maxy, maxz);
 
-#if 0
     glBindTexture(GL_TEXTURE_2D, 0);
     blit_fbo_bind(FBO_WID);
     glBlendFunc(GL_ZERO, GL_SRC_COLOR);
     blit_fbo(FBO_VISITED_MAP_MERGED);
-#endif
-glcolor(RED);
-map_light_ray_effect(0, 0);
 }
 
 void thing_map_test(void)
