a360ac03e9110e914f1e3576625c97dd324353b1
Neil McGill <goblinhack@gmail.com>
2019-01-01 20:27:42 -0500

    floor tiles

--- src/my_thing_template.h
+++ src/my_thing_template.h
@@ -193,6 +193,7 @@ Tpp tp_find_short_name(std::string name);
 void tp_init_after_loading(void);
 Tpp tp_get_random_monst(void);
 Tpp tp_get_random_key(void);
+Tpp tp_get_random_floor(void);
 Tpp tp_get_random_deco(void);
 Tpp tp_get_random_wall_deco(void);
 
