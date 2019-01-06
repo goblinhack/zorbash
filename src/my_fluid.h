e60fa6f3de667eab9228eb6a3d7946a4b4410d62
Neil McGill <goblinhack@gmail.com>
2019-01-06 15:31:55 -0500

    missing files
    Former-commit-id: 10a23a8aaf41e107eba122abc2cc7b5db85c2156

---
+++ src/my_fluid.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2017 goblinhack@gmail.com
+ *
+ * See the README file for license info.
+ */
+
+#pragma once
+
+void fluid_init(void);
+void fluid_tick(void);
+void fluid_update(void);
+void fluid_add_droplets(void);
+void fluid_remove_water_radius(int x, int y, int radius);
+void fluid_debug(void);
+void fluid_pool_debug(void);
+void fluid_mass_debug(void);
