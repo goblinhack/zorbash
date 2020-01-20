707c8c1c95ebf8a037ba6799cacd325451282ade
Neil McGill <goblinhack@gmail.com>
2020-01-20 17:32:32 -0500

    alloc
    Former-commit-id: fd1fce1390be0fd050b33db4af13714edc3c44fd

--- src/my_game.h
+++ src/my_game.h
@@ -417,14 +417,4 @@ static inline Thingp thing_find (const uint32_t id)
     return (world->find_thing_ptr(id));
 }
 
-#if defined(__MINGW32__)
-// https://stackoverflow.com/questions/10862121/undefined-reference-to-posix-memalign-using-mingw32
-#define HEAP_ALLOC(var,size) \
-    var = __mingw_aligned_malloc(sizeof(lzo_align_t), size + size / 16 + 64 + 3);
-#else
-#define HEAP_ALLOC(var,size) \
-    void *var; \
-    posix_memalign(&var, sizeof(lzo_align_t), size + size / 16 + 64 + 3);
-#endif
-
 #endif
