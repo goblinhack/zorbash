bbb7fbeafb43bd5c572a84ee16a75da014138746
Neil McGill <goblinhack@gmail.com>
2020-01-20 19:37:06 -0500

    britork

--- windows/windows.sh
+++ windows/windows.sh
@@ -1,11 +1,11 @@
 
-"/c/Program Files (x86)/BitRock InstallBuilder Enterprise 19.12.0/bin/builder-cli.exe" build windows.xml 
+"/c/Program Files (x86)/BitRock InstallBuilder for Windows 19.12.0/bin/builder-cli.exe" build windows.xml 
 if [[ $? -ne 0 ]];
 then
     exit 1
 fi
 
-cp /c/Users/nmcgill/Documents/InstallBuilder/output/zorbash-0.01-windows-installer.exe ../zorbash-installer.exe
+cp /c/Users/nmcgill/Documents/InstallBuilder/output/zorbash-0-0-1-windows-installer.exe ../zorbash-installer.exe
 if [ -d /c/Users/nmcgill/Google\ Drive/ ]; then
     cp ../zorbash-installer.exe  /c/Users/nmcgill/Google\ Drive/
 fi
