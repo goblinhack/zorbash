3662edb80d7e27a868bba5f2f29a24d50da2331f
Neil McGill <goblinhack@gmail.com>
2020-01-26 11:43:49 -0500

    test

--- src/traceback.cpp
+++ src/traceback.cpp
@@ -518,8 +518,8 @@ void _backtrace2(void)
         }
 
         // Initalize memory
-        LPWSTR console_message = (LPWSTR*) new TCHAR[MaxMsgLength];
-        LPWSTR file_message = (LPWSTR*) (LPWSTR*) new TCHAR[MaxMsgLength];
+        LPWSTR console_message = (LPWSTR) new TCHAR[MaxMsgLength];
+        LPWSTR file_message = (LPWSTR) new TCHAR[MaxMsgLength];
 
         // Set some strings
         swprintf(console_message, MaxMsgLength, L">> Frame %02lu: called from: %016X Stack: %016X Frame: %016X Address return: %016X\r\n",
