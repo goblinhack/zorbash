6cf4b67656477f5ac6e7c7c529b44537b308f630
Neil McGill <goblinhack@gmail.com>
2020-01-26 20:09:53 -0500

    test
    Former-commit-id: 6f409712bbfd5b1dbc375497e34694a8c63a1bda

--- src/traceback.cpp
+++ src/traceback.cpp
@@ -138,24 +138,6 @@ void traceback_dump (void)
 }
 
 #ifdef _WIN32
-/*
-    Copyright (c) 2010 ,
-        Cloud Wu . All rights reserved.
-
-        http://www.codingnow.com
-
-    Use, modification and distribution are subject to the "New BSD License"
-    as listed at <url: http://www.opensource.org/licenses/bsd-license.php >.
-
-filename: backtrace.c
-compiler: gcc 3.4.5 (mingw-win32)
-build command: gcc -O2 -shared -Wall -o backtrace.dll backtrace.c -lbfd -liberty -limagehlp
-how to use: Call LoadLibraryA("backtrace.dll"); at beginning of your program .
-*/
-
-#define PACKAGE "your-program-name"
-#define PACKAGE_VERSION "0.1"
-
 #include <windows.h>
 #include <excpt.h>
 #include <imagehlp.h>
@@ -166,249 +148,9 @@ how to use: Call LoadLibraryA("backtrace.dll"); at beginning of your program .
 #include <stdarg.h>
 #include <string.h>
 #include <stdbool.h>
-
-#define BUFFER_MAX (16*1024)
-
-#define BFD_ERR_OK          (0)
-#define BFD_ERR_OPEN_FAIL   (1)
-#define BFD_ERR_BAD_FORMAT  (2)
-#define BFD_ERR_NO_SYMBOLS  (3)
-#define BFD_ERR_READ_SYMBOL (4)
-
-static const char *const bfd_errors[] = {
-    "",
-    "(Failed to open bfd)",
-    "(Bad format)",
-    "(No symbols)",
-    "(Failed to read symbols)",
-};
-
-struct bfd_ctx {
-    bfd * handle;
-    asymbol ** symbol;
-};
-
-struct bfd_set {
-    char * name;
-    struct bfd_ctx * bc;
-    struct bfd_set *next;
-};
-
-struct find_info {
-    asymbol **symbol;
-    bfd_vma counter;
-    const char *file;
-    const char *func;
-    unsigned line;
-};
-
-static void
-lookup_section(bfd *abfd, asection *sec, void *opaque_data)
-{
-    struct find_info *data = static_cast<struct find_info *>(opaque_data);
-
-    if (data->func)
-        return;
-
-    if (!(bfd_get_section_flags(abfd, sec) & SEC_ALLOC))
-        return;
-
-    bfd_vma vma = bfd_get_section_vma(abfd, sec);
-    if (data->counter < vma || vma + bfd_get_section_size(sec) <= data->counter)
-        return;
-
-    bfd_find_nearest_line(abfd, sec, data->symbol, data->counter - vma, &(data->file), &(data->func), &(data->line));
-}
-
-static void
-find(struct bfd_ctx * b, DWORD offset, const char **file, const char **func, unsigned *line)
-{
-    struct find_info data;
-    data.func = NULL;
-    data.symbol = b->symbol;
-    data.counter = offset;
-    data.file = NULL;
-    data.func = NULL;
-    data.line = 0;
-
-    bfd_map_over_sections(b->handle, &lookup_section, &data);
-    if (file) {
-        *file = data.file;
-    }
-    if (func) {
-        *func = data.func;
-    }
-    if (line) {
-        *line = data.line;
-    }
-}
-
-static int
-init_bfd_ctx(struct bfd_ctx *bc, const char * procname, int *err)
-{
-    bc->handle = NULL;
-    bc->symbol = NULL;
-
-    bfd *b = bfd_openr(procname, 0);
-    if (!b) {
-        if(err) { *err = BFD_ERR_OPEN_FAIL; }
-        return 1;
-    }
-
-    if(!bfd_check_format(b, bfd_object)) {
-        bfd_close(b);
-        if(err) { *err = BFD_ERR_BAD_FORMAT; }
-        return 1;
-    }
-
-    if(!(bfd_get_file_flags(b) & HAS_SYMS)) {
-        bfd_close(b);
-        if(err) { *err = BFD_ERR_NO_SYMBOLS; }
-        return 1;
-    }
-
-    void *symbol_table;
-
-    unsigned dummy = 0;
-    if (bfd_read_minisymbols(b, FALSE, &symbol_table, &dummy) == 0) {
-        if (bfd_read_minisymbols(b, TRUE, &symbol_table, &dummy) < 0) {
-            free(symbol_table);
-            bfd_close(b);
-            if(err) { *err = BFD_ERR_READ_SYMBOL; }
-            return 1;
-        }
-    }
-
-    bc->handle = b;
-    bc->symbol = static_cast<asymbol **>(symbol_table);
-
-    if(err) { *err = BFD_ERR_OK; }
-    return 0;
-}
-
-static struct bfd_ctx *
-get_bc(struct bfd_set *set , const char *procname, int *err)
-{
-    while(set->name) {
-        if (strcmp(set->name , procname) == 0) {
-            return set->bc;
-        }
-        set = set->next;
-    }
-    struct bfd_ctx bc;
-    if (init_bfd_ctx(&bc, procname, err)) {
-        return NULL;
-    }
-    set->next = (struct bfd_set *) calloc(1, sizeof(*set));
-    set->bc = (struct bfd_ctx *) malloc(sizeof(struct bfd_ctx));
-    memcpy(set->bc, &bc, sizeof(bc));
-    set->name = strdup(procname);
-
-    return set->bc;
-}
-
-static void
-_backtrace(struct bfd_set *set, int depth , LPCONTEXT context)
-{
-    char procname[MAX_PATH];
-    GetModuleFileNameA(NULL, procname, sizeof procname);
-
-    struct bfd_ctx *bc = NULL;
-    int err = BFD_ERR_OK;
-
-    STACKFRAME frame;
-    memset(&frame,0,sizeof(frame));
-
-    frame.AddrPC.Offset = context->Rip;
-    frame.AddrPC.Mode = AddrModeFlat;
-    frame.AddrStack.Offset = context->Rsp;
-    frame.AddrStack.Mode = AddrModeFlat;
-    frame.AddrFrame.Offset = context->Rbp;
-    frame.AddrFrame.Mode = AddrModeFlat;
-
-    HANDLE process = GetCurrentProcess();
-    HANDLE thread = GetCurrentThread();
-
-    SymInitialize(process, 0, false);
-
-    char symbol_buffer[sizeof(IMAGEHLP_SYMBOL) + 255];
-    char module_name_raw[MAX_PATH];
-
-#ifdef _M_IX86
-    auto platform = IMAGE_FILE_MACHINE_I386;
-#elif _M_X64
-    auto platform = IMAGE_FILE_MACHINE_AMD64;
-#elif _M_IA64
-    auto platform = IMAGE_FILE_MACHINE_IA64;
-#else
-#error "platform not supported!"
-#endif
-
-    while(StackWalk64(platform,
-        process,
-        thread,
-        &frame,
-        context,
-        0,
-        SymFunctionTableAccess,
-        SymGetModuleBase, 0)) {
-
-        --depth;
-        if (depth < 0)
-            break;
-
-        IMAGEHLP_SYMBOL *symbol = (IMAGEHLP_SYMBOL *)symbol_buffer;
-        symbol->SizeOfStruct = (sizeof *symbol) + 255;
-        symbol->MaxNameLength = 254;
-
-        DWORD64 module_base = SymGetModuleBase(process, frame.AddrPC.Offset);
-        printf("module_base %p\n", (void*)process);
-
-        const char * module_name = "[unknown module]";
-        if (module_base &&
-            GetModuleFileNameA((HINSTANCE)module_base, module_name_raw, MAX_PATH)) {
-            module_name = module_name_raw;
-        printf("module_name %s\n", module_name);
-            bc = get_bc(set, module_name, &err);
-        }
-
-        const char * file = NULL;
-        const char * func = NULL;
-        unsigned line = 0;
-
-        if (bc) {
-            find(bc,frame.AddrPC.Offset,&file,&func,&line);
-        }
-
-        if (file == NULL) {
-            DWORD64 dummy = 0;
-            if (SymGetSymFromAddr(process, frame.AddrPC.Offset, &dummy, symbol)) {
-                file = symbol->Name;
-            }
-            else {
-                file = "[unknown file]";
-            }
-        }
-        if (func == NULL) {
-            printf("%p : %s : %s %s \n",
-                (void*) frame.AddrPC.Offset,
-                module_name,
-                file,
-                bfd_errors[err]);
-        }
-        else {
-            printf("%p : %s : %s (%d) : in function (%s) \n",
-                (void*) frame.AddrPC.Offset,
-                module_name,
-                file,
-                line,
-                func);
-        }
-    }
-}
-
 #include <psapi.h>
 #include <dbghelp.h>
+
 #define STACKWALK_MAX_NAMELEN 1024
 
 typedef struct CallstackEntry
@@ -435,10 +177,13 @@ typedef enum CallstackEntryType
     lastEntry
 } CallstackEntryType;
 
-void _backtrace2(void)
+//
+// Needs .pdb filea to work and clang is not creating them for some reason
+//
+void _backtrace (void)
 {
-    HANDLE                          process = ::GetCurrentProcess();
-    HANDLE                          thread = GetCurrentThread();
+    HANDLE process = ::GetCurrentProcess();
+    HANDLE thread = GetCurrentThread();
 
     // Randomly saw this was supposed to be called prior to StackWalk so tried
     // it
@@ -528,14 +273,6 @@ void _backtrace2(void)
         MODULEINFO                  module_info;
         SecureZeroMemory(&module_info, sizeof(MODULEINFO));
 
-        // Get the file name of the file containing the function
-//        TCHAR module_buffer[MaxPath];
-//        DWORD mod_file = GetModuleFileName((HINSTANCE)module_base,
-//        module_buffer, MaxPath);
-//        if ((module_base != 0) && (mod_file != 0)) {
-//            module_info.module_name = module_buffer;
-//        }
-
         IMAGEHLP_SYMBOL64 symbol {};
         symbol.SizeOfStruct = sizeof(IMAGEHLP_SYMBOL64);
         symbol.MaxNameLength = STACKWALK_MAX_NAMELEN;
@@ -610,30 +347,11 @@ void _backtrace2(void)
     }
 }
 
-void testn (void)
+void does_not_work (void)
 {
-    CONTEXT             context;
-    STACKFRAME64        stack;
-
-    RtlCaptureContext( &context );
-    memset( &stack, 0, sizeof( STACKFRAME64 ) );
-
-    struct bfd_set *set = (struct bfd_set *) calloc(1,sizeof(*set));
-    printf("test1\n");
-    _backtrace(set, 12, &context);
-    printf("\n\n");
-
     printf("test2\n");
     _backtrace2();
     printf("\n\n");
     DIE("test");
 }
-
-void test1 (void) { testn(); }
-void test2 (void) { test1(); }
-void test3 (void) { test2(); }
-void test4 (void) { test3(); }
-void test5 (void) { test4(); }
-void test (void) { test5(); }
-
 #endif
