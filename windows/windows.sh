0c1f8bf635ecb6602ace09a234921770e2f5b91b
Neil McGill <goblinhack@gmail.com>
2020-01-28 16:11:41 -0500

    bitrocik

--- windows/windows.sh
+++ windows/windows.sh
@@ -1,9 +1,9 @@
 
-"/c/Program Files (x86)/BitRock InstallBuilder for Windows 19.12.0/bin/builder-cli.exe" build windows.xml 
+"/c/Program Files (x86)/BitRock InstallBuilder Enterprise 19.12.0/bin/builder-cli.exe" build windows.xml 
 if [[ $? -ne 0 ]];
 then
     exit 1
 fi
 
 ls -la /c/Users/nmcgill/Documents/InstallBuilder/output/
-cp /c/Users/nmcgill/Documents/InstallBuilder/output/zorbash-0.0.1-windows-installer.exe
+cp /c/Users/nmcgill/Documents/InstallBuilder/output/* ..
