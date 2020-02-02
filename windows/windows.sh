1b2b17c5bdc7fecce4a407c3eb83a7ea0f3ca699
Neil McGill <goblinhack@gmail.com>
2020-02-01 17:07:31 -0800

    windows

--- windows/windows.sh
+++ windows/windows.sh
@@ -1,9 +1,14 @@
 
+tar -c python${PYTHONVERSION}.tar --exclude __pycache__ /mingw64/lib/python${PYTHONVERSION}
+tar -xvf python${PYTHONVERSION}.tar
+
 "/c/Program Files (x86)/BitRock InstallBuilder Enterprise 19.12.0/bin/builder-cli.exe" build windows.xml 
 if [[ $? -ne 0 ]];
 then
     exit 1
 fi
+
+source python.version.sh
 
 ls -la /c/Users/nmcgill/Documents/InstallBuilder/output/
 cp /c/Users/nmcgill/Documents/InstallBuilder/output/* ..
