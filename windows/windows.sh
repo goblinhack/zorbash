feeb3acb12b453ebfed32a8fed4436639ea7a2ca
Neil McGill <goblinhack@gmail.com>
2020-02-02 08:59:29 -0500

    wind
    Former-commit-id: a372066b3609782a8b25d92ad97261fe9c341459

--- windows/windows.sh
+++ windows/windows.sh
@@ -10,20 +10,36 @@ fi
 
 echo
 echo Zipping python files
+echo ====================
 echo tar -cf python${PYTHONVERSION}.tar --exclude __pycache__ /mingw64/lib/python${PYTHONVERSION}
 tar -cf python${PYTHONVERSION}.tar --exclude __pycache__ /mingw64/lib/python${PYTHONVERSION}
-
 ls -la python${PYTHONVERSION}.tar
+
 echo
 echo Unzipping python files
+echo ======================
 echo tar -xf python${PYTHONVERSION}.tar
 tar -xf python${PYTHONVERSION}.tar
 
+mv mingw/lib/* .
+/bin/rm -rf mingw
+
+echo
+echo Final files to package
+echo ======================
+ls -la
+
+echo
+echo Package with bitrock
+echo ====================
 "/c/Program Files (x86)/BitRock InstallBuilder Enterprise 19.12.0/bin/builder-cli.exe" build windows.xml 
 if [[ $? -ne 0 ]];
 then
     exit 1
 fi
 
+echo
+echo Installer
+echo =========
 ls -la /c/Users/nmcgill/Documents/InstallBuilder/output/
 cp /c/Users/nmcgill/Documents/InstallBuilder/output/* ..
