a034428f0d21e7fe2514cbeadc439b9aa8905b6f
Neil McGill <goblinhack@gmail.com>
2020-02-02 08:56:44 -0500

    wind
    Former-commit-id: 6892fe6efedee6dc037f47a1217eee2429157f7e

--- windows/windows.sh
+++ windows/windows.sh
@@ -1,5 +1,4 @@
 
-ls -la 
 source ./python.version.sh
 if [[ "$PYTHONVERSION" = "" ]]; then
     echo "$0: python.version.sh does not have PYTHONVERSION set"
@@ -9,9 +8,15 @@ fi
 /bin/rm -rf python${PYTHONVERSION}.tar
 /bin/rm -rf python${PYTHONVERSION}
 
+echo
 echo Zipping python files
+echo tar -cf python${PYTHONVERSION}.tar --exclude __pycache__ /mingw64/lib/python${PYTHONVERSION}
 tar -cf python${PYTHONVERSION}.tar --exclude __pycache__ /mingw64/lib/python${PYTHONVERSION}
+
+ls -la python${PYTHONVERSION}.tar
+echo
 echo Unzipping python files
+echo tar -xf python${PYTHONVERSION}.tar
 tar -xf python${PYTHONVERSION}.tar
 
 "/c/Program Files (x86)/BitRock InstallBuilder Enterprise 19.12.0/bin/builder-cli.exe" build windows.xml 
