bcfe71665d3baea97300508558a11cb9bd05383e
Neil McGill <goblinhack@gmail.com>
2020-02-02 22:22:37 -0500

    no outline for wall deco
    Former-commit-id: 3634286be7733b2eb31aead594e89c7560505cdd

---
+++ src/my_gfx.h
@@ -0,0 +1,11 @@
+//
+// Copyright goblinhack@gmail.com
+// See the README file for license info.
+//
+#ifndef _MY_GFX_H
+#define _MY_GFX_H
+
+extern void gfx_init(void);
+extern void gfx_fini(void);
+
+#endif
