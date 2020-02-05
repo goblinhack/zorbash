e5970a7a137a846f16b7c48843ed963b0dca53fa
Neil McGill <goblinhack@gmail.com>
2020-02-04 20:45:51 -0500

    faster console at init
    Former-commit-id: 81176c8d6d6e7bd07b8d9dbd26023dc66144bdc3

--- python/init.py
+++ python/init.py
@@ -25,7 +25,7 @@ def load_one_plugin(filename):
     if basename(filename) == "tp.py":
         return
 
-    zx.con("PYtHON: - loading init plugin: " + filename)
+    zx.con("PYTHON: - loading init plugin: " + filename)
 
     mod_name, file_ext = os.path.splitext(os.path.split(filename)[-1])
 
