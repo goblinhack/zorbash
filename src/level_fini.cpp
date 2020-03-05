9516604d9b1e10c7f162cec7073fc4b120238741
Neil McGill <goblinhack@gmail.com>
2020-03-04 20:39:12 -0500

    mem leak

--- src/level_fini.cpp
+++ src/level_fini.cpp
@@ -28,7 +28,7 @@ void Level::fini (void)
                     LOG("clean thing %08X at %u,%u", id, x, y);
 #endif
                     auto t = thing_find(id);
-                    t->destroy();
+                    delete t;
                 }
             }
         }
