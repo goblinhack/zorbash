2f4f2a13414de51d24cac6113460a5cd8ed4462b
Neil McGill <goblinhack@gmail.com>
2020-04-25 10:50:03 -0400

    particle wip
    Former-commit-id: 5200553fb0e939331fc9c3519ac4973749ac0db0

--- src/particle.cpp
+++ src/particle.cpp
@@ -6,6 +6,130 @@
 #include "my_game.h"
 #include "my_particle.h"
 #include "my_tile.h"
+#include "my_point.h"
+
+int Level::particle_box_collision (Particlep p,
+                                   fpoint tl,
+                                   fpoint br,
+                                   fpoint *normal,
+                                   fpoint *intersect)
+{_
+    const fpoint b = (tl + br) / 2;
+    const fpoint tr(br.x, tl.y);
+    const fpoint bl(tl.x, br.y);
+    const auto r = (1.0 / TILE_WIDTH) * PARTICLE_RADIUS;
+    const auto c = p->at;
+
+    //
+    // Corner collisions, normal is at 45 degrees. Unless there is a wall.
+    //
+    if (distance(c, tl) < r) {
+        if (!level->is_wall(b.x - 1, b.y)) {
+            normal->x = c.x - tl.x;
+            normal->y = c.y - tl.y;
+CON("p %p coll %d p %f,%f b %f,%f .. %f,%f",p,  __LINE__, p->at.x, p->at.y, tl.x, tl.y, br.x, br.y);
+            return (true);
+        }
+    }
+
+    if (distance(c, tr) < r) {
+        if (!level->is_wall(b.x + 1, b.y)) {
+            normal->x = c.x - tr.x;
+            normal->y = c.y - tr.y;
+CON("p %p coll %d p %f,%f b %f,%f .. %f,%f",p,  __LINE__, p->at.x, p->at.y, tl.x, tl.y, br.x, br.y);
+            return (true);
+        }
+    }
+
+    if (distance(c, br) < r) {
+        if (!level->is_wall(b.x + 1, b.y)) {
+            normal->x = c.x - br.x;
+            normal->y = c.y - br.y;
+CON("p %p coll %d p %f,%f b %f,%f .. %f,%f",p,  __LINE__, p->at.x, p->at.y, tl.x, tl.y, br.x, br.y);
+            return (true);
+        }
+    }
+
+    if (distance(c, bl) < r) {
+        if (!level->is_wall(b.x - 1, b.y)) {
+            normal->x = c.x - bl.x;
+            normal->y = c.y - bl.y;
+CON("p %p coll %d p %f,%f b %f,%f .. %f,%f",p,  __LINE__, p->at.x, p->at.y, tl.x, tl.y, br.x, br.y);
+            return (true);
+        }
+    }
+
+    float dist;
+    if (distance_to_line(c, tl, tr, &dist, 0)) {
+        if (dist < r) {
+            goto collided;
+        }
+    }
+
+    if (distance_to_line(c, tr, br, &dist, 0)) {
+        if (dist < r) {
+            goto collided;
+        }
+    }
+
+    if (distance_to_line(c, br, bl, &dist, 0)) {
+        if (dist < r) {
+            goto collided;
+        }
+    }
+
+    if (distance_to_line(c, bl, tl, &dist, 0)) {
+        if (dist < r) {
+            goto collided;
+        }
+    }
+
+    return (false);
+
+collided:
+    fpoint delta;
+
+    if (get_line_intersection(c, b, tl, tr, intersect)) {
+        delta.x = tl.x - tr.x;
+        delta.y = tl.y - tr.y;
+        normal->x = -delta.y;
+        normal->y = delta.x;
+CON("p %p coll %d p %f,%f b %f,%f .. %f,%f",p,  __LINE__, p->at.x, p->at.y, tl.x, tl.y, br.x, br.y);
+        return (true);
+    }
+
+    if (get_line_intersection(c, b, tr, br, intersect)) {
+        delta.x = tr.x - br.x;
+        delta.y = tr.y - br.y;
+        normal->x = -delta.y;
+        normal->y = delta.x;
+CON("p %p coll %d p %f,%f b %f,%f .. %f,%f",p,  __LINE__, p->at.x, p->at.y, tl.x, tl.y, br.x, br.y);
+        return (true);
+    }
+
+    if (get_line_intersection(c, b, br, bl, intersect)) {
+        delta.x = br.x - bl.x;
+        delta.y = br.y - bl.y;
+        normal->x = -delta.y;
+        normal->y = delta.x;
+CON("p %p coll %d p %f,%f b %f,%f .. %f,%f",p,  __LINE__, p->at.x, p->at.y, tl.x, tl.y, br.x, br.y);
+        return (true);
+    }
+
+    if (get_line_intersection(c, b, bl, tl, intersect)) {
+        delta.x = bl.x - tl.x;
+        delta.y = bl.y - tl.y;
+        normal->x = -delta.y;
+        normal->y = delta.x;
+CON("p %p coll %d p %f,%f b %f,%f .. %f,%f",p,  __LINE__, p->at.x, p->at.y, tl.x, tl.y, br.x, br.y);
+        return (true);
+    }
+
+    //
+    // Particle may be inside box.
+    //
+    return (false);
+}
 
 static spoint point_to_grid (const fpoint &p)
 {
@@ -13,7 +137,7 @@ static spoint point_to_grid (const fpoint &p)
                   p.y * (PARTICLES_HEIGHT / MAP_HEIGHT));
 }
 
-static spoint particle_to_grid (const Particle *p)
+static spoint particle_to_grid (const Particlep p)
 {
     return point_to_grid(p->at);
 }
@@ -37,7 +161,7 @@ void Level::new_particle (const fpoint &at)
             continue;
         }
 
-        s->at = at;
+        s->at = at + fpoint(0, 1); // hack
         s->in_use = true;
         s->is_new = true;
         s->orig_at = at;
@@ -45,7 +169,7 @@ void Level::new_particle (const fpoint &at)
         s->velocity = fpoint(0, 0);
 
         auto angle = random_range(0, 360) * (1.0 / RAD_360);
-        float scale = 0.1;
+        float scale = 0.01;
         sincosf(angle, &s->velocity.x, &s->velocity.y);
 
         s->velocity.x *= scale;
@@ -56,7 +180,7 @@ void Level::new_particle (const fpoint &at)
     } while (tries--);
 }
 
-void Level::free_particle (Particle *p)
+void Level::free_particle (Particlep p)
 {_
     auto idx = p - getptr(all_particles, 0);
     auto s = getptr(all_particles, idx);
@@ -66,7 +190,7 @@ void Level::free_particle (Particle *p)
     s->in_use = false;
 }
 
-void Level::attach_particle (Particle *p)
+void Level::attach_particle (Particlep p)
 {_
     if (unlikely(is_oob(p->at))) {
         return;
@@ -82,7 +206,7 @@ void Level::attach_particle (Particle *p)
     }
 }
 
-void Level::detach_particle (Particle *p)
+void Level::detach_particle (Particlep p)
 {_
     auto at = particle_to_grid(p);
     auto idx = p - getptr(all_particles, 0);
@@ -95,7 +219,7 @@ void Level::detach_particle (Particle *p)
     }
 }
 
-void Level::move_particle (Particle *p, fpoint to)
+void Level::move_particle (Particlep p, fpoint to)
 {_
     auto old_at = particle_to_grid(p);
     auto new_at = point_to_grid(to);
@@ -153,6 +277,11 @@ void Level::blit_particles (const uint16_t minx, const uint16_t miny,
                     fpoint tl(a.x - dpx, a.y - dpy);
                     fpoint br(a.x + dpx, a.y + dpy);
 
+                    if (p->is_coll) {
+                        glcolorfast(RED);
+                    } else {
+                        glcolorfast(WHITE);
+                    }
                     tile_blit(ptex, tl, br);
                 }
             }
@@ -162,6 +291,32 @@ void Level::blit_particles (const uint16_t minx, const uint16_t miny,
     blit_flush();
 }
 
+void Level::collision_check_particle (Particlep p)
+{
+    p->is_coll = false;
+    const spoint c((int)p->at.x, (int)p->at.y);
+    for (auto x = c.x - 1; x <= c.x + 1; x++) {
+        for (auto y = c.y - 1; y <= c.y + 1; y++) {
+            if (unlikely(is_oob(x, y))) {
+                continue;
+            }
+            if (level->is_wall(x, y)) {
+                fpoint normal;
+                fpoint intersect;
+                if (particle_box_collision(
+                       p,
+                       fpoint((float)x - 1.0, (float)y - 1.0),
+                       fpoint((float)x + 0.0, (float)y + 0.0),
+                       &normal,
+                       &intersect)) {
+                    p->is_coll = true;
+                    return;
+                }
+            }
+        }
+    }
+}
+
 void Level::tick_particles (void)
 {
     auto sop = getptr(all_particles, 1);
@@ -169,6 +324,7 @@ void Level::tick_particles (void)
     for (auto p = sop; p <= eop; p++) {
         if (p->in_use) {
             move_particle(p, p->at + p->velocity);
+            collision_check_particle(p);
         }
     }
 }
