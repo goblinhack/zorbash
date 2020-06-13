0bb040848a279d9c0687391e77aea79e17fe61fd
Neil McGill <goblinhack@gmail.com>
2020-06-13 10:17:06 -0400

    cold coins
    Former-commit-id: 238d6edbd6a1084f98799f047506c0b06d331dab

--- src/my_particle.h
+++ src/my_particle.h
@@ -9,23 +9,26 @@
 
 #include "my_main.h"
 #include "my_point.h"
+#include "my_size.h"
 
 typedef class Particle_ {
 public:
-    Particle_(point start, point stop,
+    Particle_(point start, point stop, size sz,
               uint32_t timestamp_start, uint32_t timestamp_stop,
               Tilep tile) :
         start(start),
         stop(stop),
+        sz(sz),
         timestamp_start(timestamp_start),
         timestamp_stop(timestamp_stop),
         tile(tile) { }
 
     point start;
     point stop;
-    uint32_t timestamp_start;
-    uint32_t timestamp_stop;
-    Tilep tile;
+    size sz;
+    uint32_t timestamp_start {};
+    uint32_t timestamp_stop {};
+    Tilep tile {};
 } Particle;
 
 #endif // _MY_PARTICLE_H_
--- src/particle.cpp
+++ src/particle.cpp
@@ -12,11 +12,11 @@
 #include "my_point.h"
 #include "my_gl.h"
 
-void Level::new_particle (point start, point stop, uint32_t dur,
+void Level::new_particle (point start, point stop, size sz, uint32_t dur,
                           const Tilep tile)
 {
     uint32_t now = time_update_time_milli();
-    all_particles.push_back(Particle(start, stop, now, now + dur, tile));
+    all_particles.push_back(Particle(start, stop, sz, now, now + dur, tile));
 }
 
 void Level::display_particles (void)
@@ -48,8 +48,12 @@ void Level::display_particles (void)
             auto d = p.stop - p.start;
             point at((d.x * dt) + p.start.x, (d.y * dt) + p.start.y);
 
-            point blit_tl(at.x - (TILE_WIDTH / 2), at.y - (TILE_HEIGHT / 2));
-            point blit_br(at.x + (TILE_WIDTH / 2), at.y + (TILE_HEIGHT / 2));
+            point blit_tl(at.x - (p.sz.w / 2), at.y - (p.sz.h / 2));
+            point blit_br(at.x + (p.sz.w / 2), at.y + (p.sz.h / 2));
+
+            int oy = sin(RAD_180 * dt) * 50;
+            blit_tl.y -= oy;
+            blit_br.y -= oy;
 
             tile_blit(p.tile, blit_tl, blit_br);
 
