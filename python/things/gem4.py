5993d5b5d3553c8bddd7981bb898abfe907db2d6
Neil McGill <goblinhack@gmail.com>
2020-06-13 11:51:26 -0400

    no bouncing gems
    Former-commit-id: 5effcb3bd81473ea1895335e37adc1702416265c

--- python/things/gem4.py
+++ python/things/gem4.py
@@ -10,7 +10,6 @@ def tp_init(name, text_name):
     x.set_collision_hit_priority(1)
     x.set_collision_radius(0.40)
     x.set_gfx_animated(True)
-    x.set_gfx_bounce_always(True)
     x.set_gfx_show_outlined(True)
     x.set_gfx_small_shadow_caster(True)
     x.set_is_collectable(True)
