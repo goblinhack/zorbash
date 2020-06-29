30eb7e14270aeb0e4ec57072cdd6f7213ca471bb
Neil McGill <goblinhack@gmail.com>
2020-06-29 21:15:52 +0100

    wip brogue style
    Former-commit-id: acb4fef4024a48cda4b41aab5729a72aacefbd25

--- src/game_items.cpp
+++ src/game_items.cpp
@@ -91,38 +91,42 @@ static void game_items_wid_create (void)
 
         auto tp = t->tp();
         auto s = tp->text_name();
-        auto w = wid_new_square_button(wid_items, "item");
-        point tl = make_point(0, y);
-        point br = make_point(UI_ITEMBAR_WIDTH - 1, y);
-        wid_set_pos(w, tl, br);
-        wid_set_style(w, UI_WID_STYLE_NONE);
-        wid_set_text(w, s);
-        wid_set_text_lhs(w, true);
 
-        if (t->is_monst()) {
-            wid_set_color(w, WID_COLOR_TEXT, ORANGE);
-        } if (t->is_generator()) {
-            wid_set_color(w, WID_COLOR_TEXT, RED);
-        } else if (t->is_food()) {
-            wid_set_color(w, WID_COLOR_TEXT, GREEN);
-        } else {
-            wid_set_color(w, WID_COLOR_TEXT, GRAY);
+        {
+            auto w = wid_new_square_button(wid_items, "item");
+            point tl = make_point(0, y);
+            point br = make_point(0, y);
+            wid_set_pos(w, tl, br);
+            wid_set_style(w, UI_WID_STYLE_NONE);
+            wid_set_fg_tile(w, tile_index_to_tile(t->tile_curr));
         }
-        wid_set_color(w, WID_COLOR_BG, RED);
-        y++;
 
-        tl.y++;
-        br.y++;
-        s = string_sprintf("%d/%d",
-                           t->get_stats_health(),
-                           t->get_stats_health_max());
-        w = wid_new_square_button(wid_items, "item");
-        wid_set_pos(w, tl, br);
-        wid_set_style(w, UI_WID_STYLE_NONE);
-        wid_set_text(w, s);
-        wid_set_text_rhs(w, true);
+        {
+            auto w = wid_new_square_button(wid_items, "item");
+            point tl = make_point(1, y);
+            point br = make_point(UI_ITEMBAR_WIDTH - 1, y);
+            wid_set_pos(w, tl, br);
+            wid_set_style(w, UI_WID_STYLE_NONE);
+#if 0
+            if (t->is_monst()) {
+                wid_set_color(w, WID_COLOR_TEXT, ORANGE);
+            } if (t->is_generator()) {
+                wid_set_color(w, WID_COLOR_TEXT, RED);
+            } else if (t->is_food()) {
+                wid_set_color(w, WID_COLOR_TEXT, GREEN);
+            } else {
+                wid_set_color(w, WID_COLOR_TEXT, GRAY);
+            }
+#endif
 
-        wid_set_color(w, WID_COLOR_BG, RED);
+            s = string_sprintf("%d/%d",
+                               t->get_stats_health(),
+                               t->get_stats_health_max());
+            wid_set_text(w, s);
+            wid_set_text_rhs(w, true);
+            //wid_set_color(w, WID_COLOR_BG, BLUE);
+        }
+
         y++;
     }
 
