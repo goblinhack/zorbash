8af7ac92f71e490ee5e5dfbd03853b7932a6f637
Neil McGill <goblinhack@gmail.com>
2020-07-06 12:58:17 +0100

    sidebar
    Former-commit-id: abe4dbff2244b3b9779b6fadc2e29f0e472e2a4b

--- src/my_main.h
+++ src/my_main.h
@@ -99,7 +99,7 @@
 #define UI_SCROLL_JOY_SCALE_MAX    1.5
 #define UI_SCROLL_WHEEL_SCALE      1.0 // How much the wheel mouse moves.
 #define UI_SCROLL_WHEEL_SCALE_MAX  10
-#define UI_SIDEBAR_LEFT_WIDTH      12
+#define UI_SIDEBAR_LEFT_WIDTH      20
 #define UI_SIDEBAR_RIGHT_WIDTH     14
 #define UI_TEXT_COLOR              GRAY // Default text color
 #define UI_TEXT_COLOR_STR          "gray"
