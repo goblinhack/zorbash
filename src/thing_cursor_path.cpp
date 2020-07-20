9b6154d6fc43c862c3474af834b5113b80ca4c95
Neil McGill <goblinhack@gmail.com>
2020-07-20 09:03:14 +0100

    move do not jump

--- src/thing_cursor_path.cpp
+++ src/thing_cursor_path.cpp
@@ -22,37 +22,18 @@ bool Thing::cursor_path_pop_next_and_move (void)
             t->dead("end of life");
         } FOR_ALL_THINGS_END()
         monstp->move_path.erase(monstp->move_path.begin());
-
-        if (!move_no_shove(future_pos)) {
+        if (move_no_shove(future_pos)) {
             cursor_path_stop();
-            return (false);
-        } else {
-#if 0
-            if (attack(future_pos)) {
-                game->tick_begin();
-                return (true);
-            }
-#endif
             return (true);
         }
-    } else {
-#if 0
-        if (is_player()) {
-minicon("jump to %f,%f", level->cursor->mid_at.x,level->cursor->mid_at.y);
-            point p = make_point(level->cursor->mid_at.x,
-                                 level->cursor->mid_at.y);
-            if (try_to_jump(p)) {
-                game->tick_begin();
-                return true;
-            }
-        }
-#endif
     }
     return false;
 }
 
 void Thing::cursor_path_pop_first_move (void)
 {_
+    auto cursor = level->cursor;
+
     if (game->cursor_move_path.size()) {
         //
         // A path to the target exists.
@@ -68,28 +49,35 @@ void Thing::cursor_path_pop_first_move (void)
         }
     }
 
-    if (level->cursor) {
-        //
-        // A path to the target does not exist. Jump?
-        //
-        point p = make_point(level->cursor->mid_at.x,
-                             level->cursor->mid_at.y);
-        if (try_to_jump(p)) {
-            game->tick_begin();
-        } else if ((fabs(p.x - mid_at.x) <= 1) &&
-                   (fabs(p.y - mid_at.y) <= 1)) {
-            log("is adjacent, try to attack %f,%F",
-                level->cursor->mid_at.x, level->cursor->mid_at.y);
+    //
+    // A path to the target does not exist. Jump?
+    //
+    point future_pos = make_point(cursor->mid_at.x,
+                                  cursor->mid_at.y);
 
-            if (attack(level->cursor->mid_at)) {
-                game->tick_begin();
-            } else {
-                MINICON("You cannot jump or move there");
-            }
-        } else {
-            MINICON("You cannot jump or move there");
-        }
+    //
+    // If adjacent, try to move there. There may be no path
+    // because perhaps a monster just moved but now we can
+    // step there.
+    //
+    if ((fabs(future_pos.x - mid_at.x) <= 1) &&
+        (fabs(future_pos.y - mid_at.y) <= 1)) {
+        CON("is adjacent, attack or move to %f,%F",
+            cursor->mid_at.x, cursor->mid_at.y);
+        attack(cursor->mid_at);
+        level->cursor_path_create();
+        return;
     }
+
+    //
+    // If not adjacent, try and jump.
+    //
+    if (try_to_jump(future_pos)) {
+        game->tick_begin();
+        return;
+    }
+
+    MINICON("You cannot move there");
 }
 
 void Thing::cursor_path_stop (void)
