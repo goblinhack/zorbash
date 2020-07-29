958cd21cf70e591b3cdad6bf01e411d0413f0c0f
Neil McGill <goblinhack@gmail.com>
2020-07-29 07:42:57 +0100

    outline bug

--- python/things/punch1.py
+++ python/things/punch1.py
@@ -8,9 +8,9 @@ def init1(name, text_name):
     x.set_text_a_or_an("a");
     x.set_gfx_animated(True)
     x.set_gfx_animated_can_hflip(True)
+    x.set_gfx_animated_can_vflip(True)
     x.set_gfx_dead_anim(False)
     x.set_gfx_attack_anim(True)
-    x.set_gfx_show_outlined(True)
     x.set_is_loggable_for_unimportant_stuff(False)
     x.set_is_loggable_for_important_stuff(False)
     x.set_is_movable(True)
@@ -18,46 +18,31 @@ def init1(name, text_name):
     x.set_text_description("Punch out")
     x.set_z_depth(zx.MAP_DEPTH_WEAPON)
 
-    delay = 20
+    delay = 40
     x.set_tile(tile=name + ".1", delay_ms=delay, is_dir_down=True)
     x.set_tile(tile=name + ".2", delay_ms=delay, is_dir_down=True)
     x.set_tile(tile=name + ".3", delay_ms=delay, is_dir_down=True)
-    x.set_tile(tile=name + ".1", delay_ms=delay, is_dir_down=True)
-    x.set_tile(tile=name + ".2", delay_ms=delay, is_dir_down=True)
-    x.set_tile(tile=name + ".3", delay_ms=delay, is_dir_down=True)
-    x.set_tile(tile=name + ".2", delay_ms=delay, is_dir_down=True, is_end_of_anim = True, is_dead_on_end_of_anim=True)
+    x.set_tile(tile=name + ".4", delay_ms=delay, is_dir_down=True, is_end_of_anim = True, is_dead_on_end_of_anim=True)
 
     x.set_tile(tile=name + ".1", delay_ms=delay, is_dir_up=True)
     x.set_tile(tile=name + ".2", delay_ms=delay, is_dir_up=True)
     x.set_tile(tile=name + ".3", delay_ms=delay, is_dir_up=True)
-    x.set_tile(tile=name + ".1", delay_ms=delay, is_dir_up=True)
-    x.set_tile(tile=name + ".2", delay_ms=delay, is_dir_up=True)
-    x.set_tile(tile=name + ".3", delay_ms=delay, is_dir_up=True)
-    x.set_tile(tile=name + ".2", delay_ms=delay, is_dir_up=True, is_end_of_anim = True, is_dead_on_end_of_anim=True)
+    x.set_tile(tile=name + ".4", delay_ms=delay, is_dir_up=True, is_end_of_anim = True, is_dead_on_end_of_anim=True)
 
     x.set_tile(tile=name + ".1", delay_ms=delay, is_dir_left=True)
     x.set_tile(tile=name + ".2", delay_ms=delay, is_dir_left=True)
     x.set_tile(tile=name + ".3", delay_ms=delay, is_dir_left=True)
-    x.set_tile(tile=name + ".1", delay_ms=delay, is_dir_left=True)
-    x.set_tile(tile=name + ".2", delay_ms=delay, is_dir_left=True)
-    x.set_tile(tile=name + ".3", delay_ms=delay, is_dir_left=True)
-    x.set_tile(tile=name + ".2", delay_ms=delay, is_dir_left=True, is_end_of_anim = True, is_dead_on_end_of_anim=True)
+    x.set_tile(tile=name + ".4", delay_ms=delay, is_dir_left=True, is_end_of_anim = True, is_dead_on_end_of_anim=True)
 
     x.set_tile(tile=name + ".1", delay_ms=delay, is_dir_right=True)
     x.set_tile(tile=name + ".2", delay_ms=delay, is_dir_right=True)
     x.set_tile(tile=name + ".3", delay_ms=delay, is_dir_right=True)
-    x.set_tile(tile=name + ".1", delay_ms=delay, is_dir_right=True)
-    x.set_tile(tile=name + ".2", delay_ms=delay, is_dir_right=True)
-    x.set_tile(tile=name + ".3", delay_ms=delay, is_dir_right=True)
-    x.set_tile(tile=name + ".2", delay_ms=delay, is_dir_right=True, is_end_of_anim = True, is_dead_on_end_of_anim=True)
+    x.set_tile(tile=name + ".4", delay_ms=delay, is_dir_right=True, is_end_of_anim = True, is_dead_on_end_of_anim=True)
 
     x.set_tile(tile=name + ".1", delay_ms=delay, is_dir_none=True)
     x.set_tile(tile=name + ".2", delay_ms=delay, is_dir_none=True)
     x.set_tile(tile=name + ".3", delay_ms=delay, is_dir_none=True)
-    x.set_tile(tile=name + ".1", delay_ms=delay, is_dir_none=True)
-    x.set_tile(tile=name + ".2", delay_ms=delay, is_dir_none=True)
-    x.set_tile(tile=name + ".3", delay_ms=delay, is_dir_none=True)
-    x.set_tile(tile=name + ".2", delay_ms=delay, is_dir_none=True, is_end_of_anim = True, is_dead_on_end_of_anim=True)
+    x.set_tile(tile=name + ".4", delay_ms=delay, is_dir_none=True, is_end_of_anim = True, is_dead_on_end_of_anim=True)
 
     x.update()
 
