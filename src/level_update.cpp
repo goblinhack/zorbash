cda52d5dea8be2a43c149c1c704be03e6730e426
Neil McGill <goblinhack@gmail.com>
2020-08-04 21:40:56 +0100

    missing files
    Former-commit-id: 7d5f6674bf513ec0f5e9d2a039dde906a20203d1

---
+++ src/level_update.cpp
@@ -0,0 +1,25 @@
+//
+// Copyright goblinhack@gmail.com
+// See the README.md file for license info.
+//
+
+#include "my_game.h"
+#include "my_dungeon.h"
+#include "my_thing.h"
+
+void Level::update_all_things (void)
+{_
+    for (auto y = 0; y < MAP_HEIGHT; y++) {
+        for (auto x = 0; x < MAP_WIDTH; x++) {
+            FOR_ALL_THINGS(this, t, x, y) {
+                t->update_light();
+            } FOR_ALL_THINGS_END();
+        }
+    }
+}
+
+void Level::update (void)
+{_
+    minimap_valid = false;
+    update_all_things();
+}
