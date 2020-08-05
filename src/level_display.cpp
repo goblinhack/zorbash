c08cb9fb67a1acbb9c1010d3c543dc50139aaa6e
Neil McGill <goblinhack@gmail.com>
2020-08-05 07:02:27 +0100

    lava anim vsync
    Former-commit-id: 4cbecc86db823613dadaa8896d41445a762d33dc

--- src/level_display.cpp
+++ src/level_display.cpp
@@ -229,10 +229,21 @@ void Level::display_map (void)
 
 void Level::display_anim (void)
 {
+    static uint32_t anim_speed;
+    if (!anim_speed) {
+        anim_speed = time_get_time_ms_cached();
+    }
+
+    if ((time_get_time_ms_cached() - anim_speed) < (uint)20) {
+        return;
+    }
+
+    anim_speed = time_get_time_ms_cached();
+
     //
     // Slow timer to scroll the water.
     //
-    if (water_step1++ >= 40) {
+    if (water_step1++ >= 20) {
         water_step1 = 0;
         if (water_step2++ >= (TILE_HEIGHT * 2) - 1) {
             water_step2 = 0;
@@ -242,7 +253,7 @@ void Level::display_anim (void)
     //
     // Slow timer to scroll the deep_water.
     //
-    if (deep_water_step1++ >= 20) {
+    if (deep_water_step1++ >= 10) {
         deep_water_step1 = 0;
         if (deep_water_step2++ >= (TILE_HEIGHT * 2) - 1) {
             deep_water_step2 = 0;
@@ -252,7 +263,7 @@ void Level::display_anim (void)
     //
     // Slow timer to scroll the lava.
     //
-    if (lava_step1++ >= 5) {
+    if (lava_step1++ >= 2) {
         lava_step1 = 0;
         if (lava_step2++ >= (TILE_HEIGHT * 2) - 1) {
             lava_step2 = 0;
