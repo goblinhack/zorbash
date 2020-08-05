2eaa31bbfe503cd06a0a2b7b4b8ef2e7bf78aa91
Neil McGill <goblinhack@gmail.com>
2020-08-05 06:58:48 +0100

    vsync blood flash
    Former-commit-id: e7ec022291b1a4d5352cb52b5118b9cbfc608530

--- src/level_display_blood.cpp
+++ src/level_display_blood.cpp
@@ -21,52 +21,63 @@ void Level::display_blood (void)
         return;
     }
 
-    static int up = 4;
-    static int down = -8;
+    static int up = 8;
+    static int down = -16;
     static int delta = up;
     static int low = 20;
     static int high = 240;
     static int pulse = 1;
     static color blood(255, 255, 255, 128);
+    static auto t = "blood-0";
 
-    if (player->is_dead) {
-        blood.a = 255;
-    } else {
-        blood.a += delta * pulse;
+    static uint32_t last_pulse_when;
+    if (!last_pulse_when) {
+        last_pulse_when = time_get_time_ms_cached();
     }
-    if (blood.a > high) {
-        blood.a = high;
-        delta = down;
+
+    if ((time_get_time_ms_cached() - last_pulse_when) > (uint)50) {
+        last_pulse_when = time_get_time_ms_cached();
+
+        if (player->is_dead) {
+            blood.a = 255;
+        } else {
+            blood.a += delta * pulse;
+        }
+
+        if (blood.a > high) {
+            blood.a = high;
+            delta = down;
+        }
+        if (blood.a < low) {
+            blood.a = low;
+            delta = up;
+        }
+
+        if (pct <= THING_HEALTH_BLOODIED_PCT1) {
+            t = "blood-0";
+            pulse = 1;
+        }
+        if (pct < THING_HEALTH_BLOODIED_PCT2) {
+            t = "blood-1";
+            pulse = 2;
+        }
+        if (pct < THING_HEALTH_BLOODIED_PCT3) {
+            t = "blood-2";
+            pulse = 2;
+        }
+        if (pct < THING_HEALTH_BLOODIED_PCT4) {
+            t = "blood-3";
+            pulse = 3;
+        }
     }
-    if (blood.a < low) {
-        blood.a = low;
-        delta = up;
-    }
+
     glcolor(blood);
-
-    auto t = "blood-0";
-    if (pct <= THING_HEALTH_BLOODIED_PCT1) {
-        t = "blood-0";
-        pulse = 1;
-    }
-    if (pct < THING_HEALTH_BLOODIED_PCT2) {
-        t = "blood-1";
-        pulse = 2;
-    }
-    if (pct < THING_HEALTH_BLOODIED_PCT3) {
-        t = "blood-2";
-        pulse = 2;
-    }
-    if (pct < THING_HEALTH_BLOODIED_PCT4) {
-        t = "blood-3";
-        pulse = 3;
-    }
 
     blit_fbo_bind(FBO_MAP);
     blit_init();
     tile_blit(tile_find_mand(t),
-                point(0,0),
-                point(game->config.inner_pix_width,
-                      game->config.inner_pix_height));
+              point(0,0),
+              point(game->config.inner_pix_width,
+                    game->config.inner_pix_height));
     blit_flush();
 }
