c05970d58c07edf242579e00b2cdb0d0da8196f3
Neil McGill <goblinhack@gmail.com>
2020-08-06 07:16:12 +0100

    fix lighting issue

--- src/my_main.h
+++ src/my_main.h
@@ -44,7 +44,7 @@
 #define MAP_ROCK_BORDER            1    // Map rock border
 #define MAP_SLOTS                  32   // Max things per map slot
 #define MAP_WIDTH                  ((GRID_WIDTH*ROOM_WIDTH) + (MAP_BORDER*2))
-#define MAX_RAY_LIGHTING           180  // Max light rays cast
+#define MAX_RAY_LIGHTING           90   // Max light rays cast
 #define PLAYER_KEY_REPEAT_MS       80   // Key repeat rate
 #define ROOM_HEIGHT                13
 #define ROOM_WIDTH                 13
