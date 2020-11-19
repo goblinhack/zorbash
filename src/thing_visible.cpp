17c84e245b2efc1b97c17dae1fe2927ee7b46bf8
Neil McGill <goblinhack@gmail.com>
2020-11-19 19:36:14 +0000

    when dead, float in lava

---
+++ src/thing_visible.cpp
@@ -0,0 +1,63 @@
+//
+// Copyright goblinhack@gmail.com
+// See the README.md file for license info.
+//
+
+#include "my_main.h"
+#include "my_level.h"
+#include "my_depth.h"
+#include "my_color.h"
+#include "my_dmap.h"
+#include "my_sprintf.h"
+#include "my_thing.h"
+
+void Thing::visible (void)
+{_
+    if (!is_hidden) {
+        return;
+    }
+
+    log("is now visible");
+    is_hidden = false;
+
+    //
+    // If this thing has an owner, should the thing stay hidden?
+    //
+    auto owner = owner_get();
+    if (owner) {
+        if (this == owner->weapon_get_carry_anim()) {
+            if (owner->weapon_get_use_anim()) {
+                //
+                // Stay hidden until the weapon use is done.
+                //
+                return;
+            }
+        }
+    }
+
+    //
+    // Reveal the weapon again too.
+    //
+    if (get_weapon_id_carry_anim().ok()) {
+        auto w = level->thing_find(get_weapon_id_carry_anim());
+        if (w) { w->visible(); }
+    }
+
+    if (get_weapon_id_use_anim().ok()) {
+        auto w = level->thing_find(get_weapon_id_use_anim());
+        if (w) { w->visible(); }
+    }
+
+    auto on_fire_anim_id = get_on_fire_anim_id();
+    if (on_fire_anim_id.ok()) {_
+        auto w = level->thing_find(on_fire_anim_id);
+        if (w) { w->visible(); }
+    }
+
+    move_finish();
+}
+
+uint8_t Thing::is_visible (void) const
+{_
+    return (!is_hidden);
+}
