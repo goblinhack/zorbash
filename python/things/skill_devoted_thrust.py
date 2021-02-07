15a0e0862b117c066a7c77e69f0023087a907996
Neil McGill <goblinhack@gmail.com>
2021-02-07 14:43:48 +0000

    add
    Former-commit-id: 82badede0039d0c5ec5569013942c06b14b2de84

---
+++ python/things/skill_devoted_thrust.py
@@ -0,0 +1,16 @@
+import zx
+import tp
+
+
+def skill_devoted_thrust_init(name, text_name):
+    x = tp.Tp(name, text_name)
+    x.set_is_skill(True)
+    x.set_text_description("Devoted thrust")
+    x.set_long_text_description("Uses half of your stamina points up in one mighty strike.")
+    x.set_tile(tile=name)
+    x.update()
+
+def init():
+    skill_devoted_thrust_init(name="skill_devoted_thrust", text_name="skill_devoted_thrust")
+
+init()
