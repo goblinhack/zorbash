10d8569722b6d252f422deb0d6c891878447afec
Neil McGill <goblinhack@gmail.com>
2021-03-20 13:55:32 +0000

    strike anim

--- python/things/skill_devoted_thrust.py
+++ python/things/skill_devoted_thrust.py
@@ -12,7 +12,7 @@ def skill_devoted_thrust_init(name, text_name):
     x.set_is_skill(True)
     x.set_long_text_description("Uses half of your stamina points up in one mighty strike.\n\nCan be used multiple times, but beware, your stamina will rapidly drop to zero and you may end up unable to attack or jump away.")
     x.set_on_use_do("skill_devoted_thrust.on_use()")
-    x.set_text_on_use("You strike with a might thrust")
+    x.set_text_on_use("%%fg=yellow$You strike with a mighty thrust%%fg=reset$")
     x.set_text_description("Devoted thrust.")
     x.set_tile(tile=name)
     x.set_tile(tile=name + "_activated")
