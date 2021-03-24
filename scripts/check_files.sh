2bc011e1f9cb2cc29f5e8e7bd6744c2833680c62
Neil McGill <goblinhack@gmail.com>
2021-03-24 08:05:17 +0000

    ramdisk
    Former-commit-id: 3306c216fa71d12e4758cac60654980c79f59fc5

--- scripts/check_files.sh
+++ scripts/check_files.sh
@@ -4,8 +4,8 @@ COUNT=$(find data -newer data/gfx.tgz -type f | wc -l)
 if [[ $COUNT -gt 0 ]];
 then 
     echo $0: Need to retar graphics tarball due to updates
-    tar zcvf data/gfx.tgz data/gfx
+    (cd data ; tar zcvf gfx.tgz gfx)
 fi
 
 echo $0: Unzip graphics
-tar zxf data/gfx.tgz
+(cd data ; tar zxf gfx.tgz)
