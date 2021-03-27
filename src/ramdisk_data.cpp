e34587c85d88170b4ed25562c843e475e5cbf42a
Neil McGill <goblinhack@gmail.com>
2021-03-27 14:58:50 +0000

    split up monsts and dungeon tiles

--- src/ramdisk_data.cpp
+++ src/ramdisk_data.cpp
@@ -25,6 +25,17 @@ void ramdisk_init (void)
     }
 
     {
+        extern unsigned char *data_tiles1_monsts_and_items_tga_start_ asm("data_tiles1_monsts_and_items_tga_start_");
+        extern unsigned char *data_tiles1_monsts_and_items_tga_end_ asm("data_tiles1_monsts_and_items_tga_end_");
+        static const unsigned char *const start = (const unsigned char *const) (char*)&data_tiles1_monsts_and_items_tga_start_;
+        static const unsigned char *const end   = (const unsigned char *const) (char*)&data_tiles1_monsts_and_items_tga_end_;
+        ramdisk_t r;
+        r.data = start;
+        r.len = end - start;
+        ramdisk_data["data/gfx/tiles1_monsts_and_items.tga"] = r;
+    }
+
+    {
         extern unsigned char *data_ui_tile_bg_tga_start_ asm("data_ui_tile_bg_tga_start_");
         extern unsigned char *data_ui_tile_bg_tga_end_ asm("data_ui_tile_bg_tga_end_");
         static const unsigned char *const start = (const unsigned char *const) (char*)&data_ui_tile_bg_tga_start_;
@@ -44,6 +55,17 @@ void ramdisk_init (void)
         r.data = start;
         r.len = end - start;
         ramdisk_data["data/gfx/solid.tga"] = r;
+    }
+
+    {
+        extern unsigned char *data_tiles1_dungeon_tga_start_ asm("data_tiles1_dungeon_tga_start_");
+        extern unsigned char *data_tiles1_dungeon_tga_end_ asm("data_tiles1_dungeon_tga_end_");
+        static const unsigned char *const start = (const unsigned char *const) (char*)&data_tiles1_dungeon_tga_start_;
+        static const unsigned char *const end   = (const unsigned char *const) (char*)&data_tiles1_dungeon_tga_end_;
+        ramdisk_t r;
+        r.data = start;
+        r.len = end - start;
+        ramdisk_data["data/gfx/tiles1_dungeon.tga"] = r;
     }
 
     {
