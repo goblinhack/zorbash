ea051b1efdd038f138519dd5ff5c9cf5f2a924e1
Neil McGill <goblinhack@gmail.com>
2021-04-03 20:06:38 +0100

    wip laser
    Former-commit-id: 13b6a73b32eccbd514cab3f5bbcdc4f124a23fa9

--- src/my_laser.h
+++ src/my_laser.h
@@ -15,15 +15,13 @@
 typedef class Laser_ {
 public:
     Laser_(ThingId thing_id,
-              point start, point stop,
-              point pixel_map_at,
-              isize sz,
-              uint32_t timestamp_start, uint32_t timestamp_stop) :
+           point start, point stop,
+           point pixel_map_at,
+           uint32_t timestamp_start, uint32_t timestamp_stop) :
         id(thing_id),
         start(start),
         stop(stop),
         pixel_map_at(pixel_map_at),
-        sz(sz),
         timestamp_start(timestamp_start),
         timestamp_stop(timestamp_stop) { }
 
@@ -31,7 +29,6 @@ public:
     point start;
     point stop;
     point pixel_map_at;
-    isize sz;
     uint32_t timestamp_start {};
     uint32_t timestamp_stop {};
     int height {};
