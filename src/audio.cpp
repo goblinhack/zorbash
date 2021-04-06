4d69741f584a9ba76b5c0971d51e662644d644ed
Neil McGill <goblinhack@gmail.com>
2021-04-06 20:36:16 +0100

    add audio files

---
+++ src/audio.cpp
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2011-2020 goblinhack@gmail.com
+ *
+ * See the README file for license info for license.
+ */
+
+#include <SDL_mixer.h>
+#include "my_sys.h"
+#include "my_main.h"
+#include "my_audio.h"
+#include "my_ptrcheck.h"
+#include "my_file.h"
+#include "my_game.h"
+
+uint8_t audio_init_done;
+
+void audio_fini (void)
+{_
+    Mix_CloseAudio();
+}
+
+bool audio_init (void)
+{_
+    //
+    // Output sampling frequency in samples per second (Hz).
+    // you might use MIX_DEFAULT_FREQUENCY(22050) since that is a good value 
+    // for most games.
+    //
+    int audio_freq = MIX_DEFAULT_FREQUENCY;
+    int audio_format = MIX_DEFAULT_FORMAT;
+
+    //
+    // Number of sound channels in output.
+    // Set to 2 for stereo, 1 for mono. This has nothing to do with 
+    // mixing channels. 
+    //
+    int channels = 2;
+
+    //
+    // chunksize is the size of each mixed sample. The smaller this is the 
+    // more your hooks will be called. If make this too small on a slow 
+    // system, sound may skip. If made to large, sound effects will lag 
+    // behind the action more. You want a happy medium for your target 
+    // computer. You also may make this 4096, or larger, if you are just 
+    // playing music. 
+    //
+    int chunksize = 4096;
+
+    if (!audio_init_done) {
+        if (Mix_OpenAudio(audio_freq,
+                          audio_format,
+                          channels,
+                          chunksize) != 0) {
+
+            ERR("Mix_OpenAudio fail: %s %s", Mix_GetError(), SDL_GetError());
+            SDL_ClearError();
+            return false;
+        }
+
+        audio_init_done = true;
+    }
+
+    return true;
+}
---
+++ src/my_audio.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2011-2020 goblinhack@gmail.com
+ *
+ * See the README file for license info.
+ */
+
+#pragma once
+
+#ifndef _MY_AUDIO_H_
+#define _MY_AUDIO__H_
+
+bool audio_init(void);
+void audio_fini(void);
+
+#endif
