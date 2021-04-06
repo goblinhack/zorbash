0861d188916d6a9b9b272c66f228c9520c1ad4f4
Neil McGill <goblinhack@gmail.com>
2021-04-06 09:03:21 +0100

    minor and major explosions
    Former-commit-id: ec812f0ff50010c5a317cbffe62eb351759e1312

--- python/things/cursor_select.py
+++ python/things/cursor_select.py
@@ -14,7 +14,7 @@ def tp_init(name):
     x.set_is_moveable(True)
     x.set_text_a_or_an("the");
     x.set_text_description("Cursor.")
-    x.set_z_depth(zx.MAP_DEPTH_EXPLOSION)
+    x.set_z_depth(zx.MAP_DEPTH_EXPLOSION_MINOR)
     x.set_z_prio(zx.MAP_PRIO_BEHIND)
 
     delay = 50
