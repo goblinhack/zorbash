e02c5cfd9fe491135b32da2ae57cc8327587017b
Neil McGill <goblinhack@gmail.com>
2021-04-12 20:02:16 +0100

    fix width of thing info

--- python/init.py
+++ python/init.py
@@ -5,7 +5,22 @@ from os.path import dirname, basename
 import imp
 import sys
 import console
+import time
 
+timeme_enabled = True
+
+def timeme(py_function):
+    def timeme_wrapper(*args, **kwargs):
+        global timeme_enabled
+        if not timeme_enabled:
+            return py_function(*args, **kwargs)
+
+        start_time = time.time()
+        return_code = py_function(*args, **kwargs)
+        end_time = time.time()
+        print('PYC: perf {:s}({}) {:.3f} ms'.format(py_function.__name__, args, (end_time - start_time) * 1000.0))
+        return return_code
+    return timeme_wrapper
 
 def find_plugins(directory, pattern):
     for root, dirs, files in os.walk(directory):
@@ -15,6 +30,7 @@ def find_plugins(directory, pattern):
                 yield filename
 
 
+@timeme
 def load_one_plugin(filename):
     if "rollbackBackupDirectory" in filename:
         return
@@ -39,6 +55,7 @@ def load_one_plugin(filename):
     elif file_ext.lower() == '.pyc':
         py_mod = imp.load_compiled(mod_name, filename)
 
+@timeme
 def load_all_plugins():
     plug_path = os.path.normcase(os.path.join(dirname(__file__), ""))
     zx.log("PYC: Init module, load all plugins from " + plug_path)
@@ -46,10 +63,12 @@ def load_all_plugins():
         load_one_plugin(filename)
 
 
+@timeme
 def load_plugin(plugin):
     for filename in find_plugins(os.getcwd(), plugin):
         load_one_plugin(filename)
 
+@timeme
 def init1():
     zx.log("PYC: Init module: running in: {}".format(os.getcwd()))
 
