75a7bd235a105164c4ea13a2e12af3458b671f69
Neil McGill <goblinhack@gmail.com>
2021-05-08 09:35:09 +0100

    wip wid enchant
    Former-commit-id: 184c029d488376289a3c0b9458ebc900dd089fe9

---
+++ src/my_wid_thing_enchant.h
@@ -0,0 +1,16 @@
+//
+// Copyright goblinhack@gmail.com
+// See the README.md file for license info.
+//
+
+#pragma once
+void wid_thing_enchant_fini(void);
+uint8_t wid_thing_enchant_init(void);
+
+#include "my_wid.h"
+#include "my_wid_popup.h"
+
+//
+// Global widgets.
+//
+extern WidPopup *wid_thing_enchant_window;
---
+++ src/wid_thing_enchant.cpp
@@ -0,0 +1,131 @@
+//
+// Copyright goblinhack@gmail.com
+// See the README.md file for license info.
+//
+
+#include "my_sys.h"
+#include "my_game.h"
+#include "my_wid_topcon.h"
+#include "my_wid_botcon.h"
+#include "my_wid_popup.h"
+#include "my_wid_thing_enchant.h"
+#include "my_tile.h"
+#include "my_wid_console.h"
+#include "my_wid_thing_info.h"
+#include "my_wid_bag.h"
+#include "my_level.h"
+#include "my_thing.h"
+#include "my_monst.h"
+
+WidPopup *wid_thing_enchant_window;
+
+void wid_thing_enchant_fini (void)
+{_
+    LOG("Thing enchant fini");
+
+    if (game->bag_primary) {
+        delete game->bag_primary;
+        game->bag_primary = nullptr;
+    }
+
+    delete wid_thing_enchant_window;
+    wid_thing_enchant_window = nullptr;
+}
+
+uint8_t wid_thing_enchant_init (void)
+{_
+    return true;
+}
+
+void Game::wid_thing_enchant_destroy (void)
+{_
+    LOG("Thing enchant destroy");
+
+    if (game->request_remake_inventory) {
+        //
+        // Continue
+        //
+    } else if (game->state == Game::STATE_ENCHANTING_ITEMS) {
+        return;
+    }
+
+    wid_thing_enchant_fini();
+}
+
+void Game::wid_thing_enchant_create (const std::list<Thingp> &items)
+{_
+    LOG("Thing enchant create");
+_
+    if (game->request_remake_inventory) {
+        //
+        // Continue
+        //
+        LOG("Remake thing enchant");
+    } else if (game->state == Game::STATE_ENCHANTING_ITEMS) {
+        LOG("Ignore, already enchanting items");
+        return;
+    }
+
+    if (game->in_transit_item) {
+        LOG("Ignore, already in transit item0");
+        return;
+    }
+
+    if (wid_console_window && wid_console_window->visible) {
+        LOG("Console visible");
+        return;
+    }
+
+    if (wid_thing_enchant_window) {
+        LOG("Destroy window");
+        wid_thing_enchant_destroy();
+    }
+
+    auto player = game->level->player;
+    if (!player){
+        ERR("No player");
+        return;
+    }
+
+    LOG("Thing enchant create bags");
+
+    point mid(TERM_WIDTH / 2, TERM_HEIGHT - 1);
+
+    if (bag_primary) {
+        delete bag_primary;
+        bag_primary = nullptr;
+    }
+
+    change_state(Game::STATE_ENCHANTING_ITEMS);
+
+    //
+    // Might as well show something; the player
+    //
+    if (player) {
+        wid_thing_info_push_popup(player);
+    }
+
+    {
+        auto volume = bag_estimate_volume(items);
+        auto height = sqrt(volume);
+        if (height >= MAX_BAG_WIDTH) {
+            height = MAX_BAG_WIDTH;
+            ERR("Bag size is too large");
+        }
+
+        if (height < sizeof("Items found")) {
+            height = sizeof("Items found");
+        }
+
+        point tl = mid + point(0, - (height + 1));
+        point br = tl +  point(height, height);
+
+        auto temporary_bag = level->thing_new("bag_items", fpoint(-1, -1));
+        temporary_bag->new_monst();
+        temporary_bag->monstp->bag_width = height;
+        temporary_bag->monstp->bag_height = height;
+        temporary_bag->try_to_carry(items);
+
+        bag_primary = new WidBag(temporary_bag, tl, br, "Items found");
+    }
+}
