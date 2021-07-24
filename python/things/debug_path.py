e37024d9539bd8002287cc54b4d4508392db7709
Neil McGill <goblinhack@gmail.com>
2021-07-24 11:06:34 +0100

    ai debug path
    Former-commit-id: 39ef7ae04a3da9a692fa9f246a4dbe4752490b12

---
+++ python/things/debug_path.py
@@ -0,0 +1,26 @@
+import zx
+import tp
+
+
+def tp_init(name):
+    text_name = "debug path"
+    mytp = tp.Tp(name, text_name)
+    mytp.set_gfx_animated(True)
+    mytp.set_is_able_to_fall(False)
+    mytp.set_is_debug_path(True)
+    mytp.set_is_floating(True)
+    mytp.set_is_moveable(True)
+    mytp.set_is_removeable_if_out_of_slots(True)
+    mytp.set_text_a_or_an("the")
+    mytp.set_text_description("debug path.")
+    mytp.set_z_depth(zx.MAP_DEPTH_OBJ)
+    mytp.set_z_prio(zx.MAP_PRIO_BEHIND)
+
+    mytp.set_tile("ai_path1.1")
+
+    mytp.update()
+
+def init():
+    tp_init(name="debug_path")
+
+init()
