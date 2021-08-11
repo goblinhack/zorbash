1de3d528d12bbae898a14a27ba428720dbda4f0d
Neil McGill <goblinhack@gmail.com>
2021-08-11 16:59:59 +0100

    do not jump into chasms when robot and at end of move
    Former-commit-id: ed76490944822dfdeb1fd391e3ba75077f7ff18c

--- src/thing_cursor_path.cpp
+++ src/thing_cursor_path.cpp
@@ -51,7 +51,7 @@ bool Thing::cursor_path_pop_next_and_move (void)
         //
         if (is_player() && game->robot_mode) {
             if (will_avoid_threat(future_pos)) {
-                log("Next position %d,%d is a threat",
+                CON("Next position %d,%d is a threat",
                     (int)future_pos.x, (int)future_pos.y);
 _
                 if (monstp->move_path.size()) {
@@ -62,7 +62,7 @@ _
                     // If the thing we are going to land on is also a threat,
                     // can we jump further?
                     //
-                    log("Next next position %d,%d is also a threat",
+                    CON("Next next position %d,%d is also a threat",
                         (int)future_pos.x, (int)future_pos.y);
 _
                     if (will_avoid_threat(jump_pos) && monstp->move_path.size()) {
@@ -91,6 +91,9 @@ _
                         clear_move_path("Player cannot pass");
                         return false;
                     }
+                } else {
+                    clear_move_path("Player cannot pass");
+                    return false;
                 }
             }
         }
