476544dac9fb4ae11e8d357b4641d649a9bebc24
Neil McGill <goblinhack@gmail.com>
2021-08-25 19:57:13 +0100

    pre adding tmp things
    Former-commit-id: 0948356eb8f2001f961cfd242664c4f6c6c8ee7c

--- src/sdl.cpp
+++ src/sdl.cpp
@@ -1259,18 +1259,18 @@ void sdl_loop (void)
                     }
                     pcg_random_allowed = true;
                 } else {
-                    //
-                    // Must do this before wid_display_all so that the
-                    // on screen widgets are updated with the new wid
-                    // we are about to make.
-                    //
-                    if (game->request_update_rightbar) {
-                        game->request_update_rightbar = false;
-                        wid_rightbar_init();
-                    }
-
                     pcg_random_allowed = false;
                     {
+                        //
+                        // Must do this before wid_display_all so that the
+                        // on screen widgets are updated with the new wid
+                        // we are about to make.
+                        //
+                        if (game->request_update_rightbar) {
+                            game->request_update_rightbar = false;
+                            wid_rightbar_init();
+                        }
+
                         wid_display_all();
                     }
                     pcg_random_allowed = true;
