85333b1bd2a9960485912357e23f98fd8a3db0a1
Neil McGill <goblinhack@gmail.com>
2021-10-15 17:41:36 +0100

    new file
    Former-commit-id: 1641c67546ac89ea57142b2e117be8eddae356e4

---
+++ python/things/buff_poison_resistant.py
@@ -0,0 +1,25 @@
+import zx
+import tp
+
+mytp = None
+
+def on_use(owner, debuff, target, x, y):
+    zx.topcon("%%fg=green$You are immune to poison!")
+
+def buff_poison_resistant_init(name, text_name):
+    global mytp
+    mytp = tp.Tp(name, text_name)
+    mytp.set_is_loggable(True)
+    mytp.set_is_buff(True)
+    mytp.set_text_buff("You are poison resistant");
+    mytp.set_long_text_description("Poison cannot harm you.")
+    mytp.set_on_use_do("buff_poison_resistant.on_use()")
+    mytp.set_text_description("poison_resistant buff.")
+    #mytp.set_tile(tile="buff_poison_resistant")
+    mytp.set_tile(tile="debuff_poisoned")
+    mytp.update()
+
+def init():
+    buff_poison_resistant_init(name="buff_poison_resistant", text_name="poison_resistant")
+
+init()
