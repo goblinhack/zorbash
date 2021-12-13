408e6db9c00952dc74b851489cc88a32d8165d15
Neil McGill <goblinhack@gmail.com>
2021-12-13 18:49:04 +0000

    wip polymorph
    Former-commit-id: b617a8c8bd445636e0c19ea1384d424cafa26077

---
+++ src/thing_polymorph.cpp
@@ -0,0 +1,85 @@
+//
+// Copyright goblinhack@gmail.com
+// See the README.md file for license info.
+//
+
+#include "my_array_bounds_check.hpp"
+#include "my_backtrace.hpp"
+#include "my_color.hpp"
+#include "my_depth.hpp"
+#include "my_dmap.hpp"
+#include "my_game.hpp"
+#include "my_level.hpp"
+#include "my_ptrcheck.hpp"
+#include "my_python.hpp"
+#include "my_random.hpp"
+#include "my_sprintf.hpp"
+#include "my_string.hpp"
+#include "my_sys.hpp"
+#include "my_thing.hpp"
+#include "my_thing_template.hpp"
+
+void Thing::on_polymorphed(void)
+{
+  TRACE_AND_INDENT();
+  auto on_polymorphed = tp()->on_polymorphed_do();
+  if (std::empty(on_polymorphed)) {
+    return;
+  }
+
+  auto t = split_tokens(on_polymorphed, '.');
+  if (t.size() == 2) {
+    auto        mod   = t[ 0 ];
+    auto        fn    = t[ 1 ];
+    std::size_t found = fn.find("()");
+    if (found != std::string::npos) {
+      fn = fn.replace(found, 2, "");
+    }
+
+    dbg("Call %s.%s(%s, %d, %d)", mod.c_str(), fn.c_str(), to_string().c_str(), (int) mid_at.x, (int) mid_at.y);
+
+    py_call_void_fn(mod.c_str(), fn.c_str(), id.id, (unsigned int) mid_at.x, (unsigned int) mid_at.y);
+  } else {
+    ERR("Bad on_polymorphed call [%s] expected mod:function, got %d elems", on_polymorphed.c_str(),
+        (int) on_polymorphed.size());
+  }
+}
+
+void Thing::polymorph(Tpp into)
+{
+  verify(MTYPE_THING, this);
+  log("Polymorpgh into: %s", into->name().c_str());
+
+  tp_id = into->id;
+  tp_or_update();
+
+  //
+  // Make sure we have the ability to carry items.
+  //
+  if (is_player() || is_monst() || is_item() || is_cursor()) {
+    new_infop();
+  }
+
+  if (is_player() || is_monst() || is_item()) {
+    new_itemp();
+  }
+
+  if (is_player() || is_monst() || is_cursor()) {
+    new_aip();
+  }
+
+  if (! is_item_collector()) {
+    drop_all();
+  }
+
+  //
+  // Init the z depth
+  //
+  level_pop();
+  z_depth = tp()->z_depth;
+  level_push();
+
+  update();
+
+  on_polymorphed();
+}
