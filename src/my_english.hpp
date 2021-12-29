3e14bb905090abf040ea7b39f12eba594c6c025b
Neil McGill <goblinhack@gmail.com>
2021-12-29 16:54:26 +0000

    add pluralise from crawl

---
+++ src/my_english.hpp
@@ -0,0 +1,14 @@
+//
+// Borrowed from "crawl"
+//
+#pragma once
+#ifndef _MY_ENGLISH_HPP_
+#define _MY_ENGLISH_HPP_
+#include <string>
+
+extern bool starts_with(const std::string &s, const std::string &prefix);
+extern bool ends_with(const std::string &s, const std::string &suffix);
+
+extern std::string pluralise(const std::string &name);
+extern std::string apostrophise(const std::string &name);
+#endif
