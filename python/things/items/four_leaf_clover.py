6ec2fcb4628e04ef03926f65e5f7ec5328e17ae4
Neil McGill <goblinhack@gmail.com>
2022-02-19 13:47:32 +0000

    four leaf clover
    Former-commit-id: beb9ffd42247a9fc8a4d82ce6ed8b711daf9e9b7

---
+++ python/things/items/four_leaf_clover.py
@@ -0,0 +1,48 @@
+import my
+import tp
+
+
+def tp_init(name, text_name, short_text_name):
+    self = tp.Tp(name, text_name, short_text_name)
+    my.gold_value_dice(self, "1")
+    my.is_able_to_fall(self, True)
+    my.is_bag_item(self, True)
+    my.is_biome_dungeon(self, True)
+    my.is_biome_swamp(self, True)
+    my.is_collectable(self, True)
+    my.is_described_when_hovering_over(self, True)
+    my.is_droppable(self, True)
+    my.is_enchantable(self, True)
+    my.is_interesting(self, True)
+    my.is_item(self, True)
+    my.is_loggable(self, True)
+    my.is_moveable(self, True)
+    my.is_treasure_class_a(self, True)
+    my.is_food(self, True)
+    my.is_crushable(self, True)
+    my.is_treasure(self, True)
+    my.is_treasure_type(self, True)
+    my.item_height(self, 2)
+    my.item_width(self, 2)
+    my.nutrition_dice(self, "1d40")
+    my.long_text_description(self, "Nutritious and lucky!")
+    my.noise_on_moving_or_being_carried(self, 25)
+    my.rarity(self, my.RARITY_UNCOMMON)
+    my.normal_placement_rules(self, True)
+    my.stat_luck_mod(self, 4)
+    my.text_a_or_an(self, "a")
+    my.text_description(self, "A four leaf clover.")
+    my.text_enchant(self, "+1 LUCK")
+    my.z_depth(self, my.MAP_DEPTH_OBJ)
+    my.z_prio(self, my.MAP_PRIO_BEHIND)
+
+    my.tile(self, tile=name, delay_ms=100)
+
+    my.tp_update(self)
+
+
+def init():
+    tp_init(name="four_leaf_clover", text_name="four-leaf clover", short_text_name="four-leaf clover")
+
+
+init()
