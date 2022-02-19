018ecd9ca4c0620c77ea51bb4e0132cf5fea8e99
Neil McGill <goblinhack@gmail.com>
2022-02-19 13:38:04 +0000

    fix axe carry and enchant
    Former-commit-id: 9c618ba5cd8c64f79b9d3eba829d6ff3258cee4f

--- python/things/weapons/axe_carry.py
+++ python/things/weapons/axe_carry.py
@@ -6,9 +6,11 @@ def init_carry(name):
     self = tp.Tp(name)
     my.gfx_animated_can_hflip(self, True)
     my.gfx_animated(self, True)
+    my.gfx_bounce_on_move(self, True)
     my.gfx_equip_carry_anim(self, True)
     my.gfx_short_shadow_caster(self, True)
     my.gfx_show_outlined(self, True)
+    my.is_gfx_anim_synced_with_owner(self, True)
     my.is_loggable(self, True)
     my.is_moveable(self, True)
     my.text_a_or_an(self, "a")
@@ -16,9 +18,14 @@ def init_carry(name):
     my.z_depth(self, my.MAP_DEPTH_EQUIP)
     my.z_prio(self, my.MAP_PRIO_NORMAL)
 
-    delay = 550
-    my.tile(self, tile=name + ".1", delay_ms=delay)
-    my.tile(self, tile=name + ".2", delay_ms=delay)
+    # This should be synced with its owner, but just in case it is not, we have delay set
+    delay = 900
+    my.tile(self, tile=name + ".1", delay_ms=delay, frame=1)
+    my.tile(self, tile=name + ".2", delay_ms=delay, frame=2)
+    my.tile(self, tile=name + ".3", delay_ms=delay, frame=3)
+    my.tile(self, tile=name + ".4", delay_ms=delay, frame=4)
+    my.tile(self, tile=name + ".5", delay_ms=delay, frame=5)
+    my.tile(self, tile=name + ".6", delay_ms=delay, frame=6)
 
     my.tp_update(self)
 
