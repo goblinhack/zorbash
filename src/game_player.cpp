b2985b13a367d8db0cf34f55d7699623c24ae14c
Neil McGill <goblinhack@gmail.com>
2022-02-26 10:19:37 +0000

    very broadsword
    Former-commit-id: fbe19459851470c7f4993d2ef0d471a942f051b1

--- src/game_player.cpp
+++ src/game_player.cpp
@@ -46,6 +46,11 @@ void Game::place_player(void)
         t->carry(w);
         t->enchant_without_stone(w);
       }
+      if (0) {
+        auto w = level->thing_new("staff_lightning", point(x, y));
+        t->carry(w);
+        t->enchant_without_stone(w);
+      }
       if (1) {
         auto w = level->thing_new("scythe", point(x, y));
         t->carry(w);
