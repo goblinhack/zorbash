3a3c853b9071a76edffd225f87720fae2c49f190
Neil McGill <goblinhack@gmail.com>
2022-03-06 15:26:54 +0000

    no cold explosion into fire
    Former-commit-id: 5f3c7c8061d5bdd0f2365d12a131d474e962d767

--- python/things/effects/explosion_cold.py
+++ python/things/effects/explosion_cold.py
@@ -8,16 +8,15 @@ def on_death(me, x, y):
 
 def tp_init(name):
     self = tp.Tp(name)
-    my.damage_fire_chance_d1000(self, 1000)
-    my.damage_fire_dice(self, "1d6")  # This is the damage when the monst fires
+    my.damage_cold_chance_d1000(self, 1000)
+    my.damage_cold_dice(self, "1d6")  # This is the damage when the monst fires
     my.gfx_animated(self, True)
     my.gfx_short_shadow_caster(self, True)
     my.gfx_show_outlined(self, True)
     my.is_explosion(self, True)
-    my.is_fire(self, True)
+    my.is_cold(self, True)
     my.is_floating(self, True)
     my.is_loggable(self, True)
-    my.is_tickable(self, True)
     my.is_tickable(self, True)  # So it can interact with fire
     my.noise_on_born(self, 70)
     my.on_death_do(self, "me.on_death()")
@@ -27,21 +26,20 @@ def tp_init(name):
 
     my.tile(self, tile="nothing")
     delay = 25
-    my.tile(self, "explosion_minor.1", delay_ms=delay)
-    my.tile(self, "explosion_minor.2", delay_ms=delay)
-    my.tile(self, "explosion_minor.3", delay_ms=delay)
+    my.tile(self, "explosion_fire.1", delay_ms=delay)
+    my.tile(self, "explosion_fire.2", delay_ms=delay)
+    my.tile(self, "explosion_fire.3", delay_ms=delay)
     my.tile(self,
-        "explosion_minor.4",
-        delay_ms=delay,
-        is_end_of_anim=True,
-        is_dead_on_end_of_anim=True,
-    )
+            "explosion_fire.4",
+            delay_ms=delay,
+            is_end_of_anim=True,
+            is_dead_on_end_of_anim=True)
 
     my.tp_update(self)
 
 
 def init():
-    tp_init(name="explosion_minor")
+    tp_init(name="explosion_cold")
 
 
 init()
