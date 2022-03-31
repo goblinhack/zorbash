f9b400943abf6d8b2f4bcc81c6d849872ed0d334
Neil McGill <goblinhack@gmail.com>
2022-03-31 08:04:29 +0100

    fix double punch

--- src/game_player.cpp
+++ src/game_player.cpp
@@ -83,7 +83,7 @@ void Game::place_player(void)
         t->carry(w);
         t->enchant_without_stone(w);
       }
-      if (1) {
+      if (0) {
         auto w = level->thing_new("sword_wood", point(x, y));
         t->carry(w);
       }
