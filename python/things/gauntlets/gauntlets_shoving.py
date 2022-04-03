8bcd9e203dd1a4f3458408640aade426b77d4169
Neil McGill <goblinhack@gmail.com>
2022-04-03 09:58:45 +0100

    gauntlets of shoving

---
+++ python/things/gauntlets/gauntlets_shoving.py
@@ -0,0 +1,76 @@
+import my
+import tp
+
+self = None
+
+
+def on_equip(owner, me, x, y):
+    if my.thing_is_player(owner):
+        my.thing_msg(me, "Your fists feel powerful!")
+
+
+def on_unequip(owner, me, x, y):
+    if my.thing_is_player(owner):
+        my.thing_msg(me, "Your fists feel like normal fists again!")
+
+
+def on_enchant(me, x, y):
+    owner = my.thing_top_owner_id_get(me)
+    if my.thing_is_player(owner):
+        my.thing_msg(me, "The gauntlets become yet more menacing.")
+    my.thing_shove_strength_mod_incr(me, 1)
+
+
+def tp_init(name, text_name, short_text_name):
+    global self
+    self = tp.Tp(name, text_name, short_text_name)
+    my.equip_carry_anim(self, "gauntlets_shoving_carry")
+    my.gfx_short_shadow_caster(self, True)
+    my.gold_value_dice(self, "50")
+    my.is_able_to_fall(self, True)
+    my.is_auto_equipped(self, True)
+    my.is_bag_item(self, True)
+    my.is_biome_dungeon(self, True)
+    my.is_biome_swamp(self, True)
+    my.is_gauntlet(self, True)
+    my.is_collectable(self, True)
+    my.is_described_when_hovering_over(self, True)
+    my.is_droppable(self, True)
+    my.is_enchantable(self, True)
+    my.is_interesting(self, True)
+    my.is_item(self, True)
+    my.is_loggable(self, True)
+    my.is_treasure_class_a(self, True)
+    my.is_treasure(self, True)
+    my.is_treasure_type(self, True)
+    my.item_height(self, 3)
+    my.item_width(self, 3)
+    my.long_text_description(self, "Gauntlet of shoving. No longer will you get pushed around with this slight boost to your shoving ability! Works in both offensive and defensive capabilities. Not that you would ever try to shove something into a chasm... right?")
+    my.noise_additional_on_jump_end(self, -50)
+    my.noise_on_dropping(self, -50)
+    my.normal_placement_rules(self, True)
+    my.on_enchant_do(self, "me.on_enchant()")
+    my.on_equip_do(self, "me.on_equip()")
+    my.shove_strength_mod(self, 1)
+    my.on_unequip_do(self, "me.on_unequip()")
+    my.rarity(self, my.RARITY_UNCOMMON)
+    my.text_a_or_an(self, "a")
+    my.text_description(self, "Gauntlets of shoving.")
+    my.text_enchant(self, "-10 decibels")
+    my.z_depth(self, my.MAP_DEPTH_OBJ)
+    my.z_prio(self, my.MAP_PRIO_BEHIND)
+
+    my.tile(self, tile=name)
+
+    my.tp_update(self)
+
+
+def init():
+    tp_init(
+        name="gauntlets_shoving",
+        text_name="gauntlets of shoving",
+        short_text_name="gauntlets of shoving",
+    )
+
+
+init()
