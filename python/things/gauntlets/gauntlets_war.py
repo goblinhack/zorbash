48f836ffb3e372aaf8f14f1cfd66664f1af13f8e
Neil McGill <goblinhack@gmail.com>
2022-04-03 20:39:10 +0100

    gauntlets of war
    Former-commit-id: 5257642ed63532e9b0e4377d2f9ac666e0dff9b0

---
+++ python/things/gauntlets/gauntlets_war.py
@@ -0,0 +1,78 @@
+import my
+import tp
+
+self = None
+
+
+def on_equip(owner, me, x, y):
+    if my.thing_is_player(owner):
+        my.thing_msg(me, "Your fists feel full of rage!")
+
+
+def on_unequip(owner, me, x, y):
+    if my.thing_is_player(owner):
+        my.thing_msg(me, "Your fists feel like normal fists again!")
+
+
+def on_enchant(me, x, y):
+    owner = my.thing_top_owner_id_get(me)
+    if my.thing_is_player(owner):
+        my.thing_msg(me, "The gauntlets become yet more war like.")
+    my.thing_shove_strength_mod_incr(me, 1)
+
+
+def tp_init(name, text_name, short_text_name):
+    global self
+    self = tp.Tp(name, text_name, short_text_name)
+    my.equip_carry_anim(self, "gauntlets_war_carry")
+    my.gfx_short_shadow_caster(self, True)
+    my.gold_value_dice(self, "1050")
+    my.is_able_to_fall(self, True)
+    my.is_auto_equipped(self, True)
+    my.is_bag_item(self, True)
+    my.is_biome_dungeon(self, True)
+    my.is_biome_swamp(self, True)
+    my.is_collectable(self, True)
+    my.is_described_when_hovering_over(self, True)
+    my.is_droppable(self, True)
+    my.is_enchantable(self, True)
+    my.is_gauntlet(self, True)
+    my.is_interesting(self, True)
+    my.is_item(self, True)
+    my.is_loggable(self, True)
+    my.is_treasure_class_c(self, True)
+    my.is_treasure(self, True)
+    my.is_treasure_type(self, True)
+    my.item_height(self, 3)
+    my.item_width(self, 3)
+    my.long_text_description(self, "Gauntlets of war. Providing a hefty strength and defense bonus, these gauntlets mean business.?")
+    my.noise_additional_on_jump_end(self, -50)
+    my.noise_on_dropping(self, -50)
+    my.noise_on_moving(self, 25)
+    my.normal_placement_rules(self, True)
+    my.on_enchant_do(self, "me.on_enchant()")
+    my.on_equip_do(self, "me.on_equip()")
+    my.on_unequip_do(self, "me.on_unequip()")
+    my.rarity(self, my.RARITY_VERY_RARE)
+    my.stat_def_mod(self, 3)
+    my.stat_str_mod(self, 5)
+    my.text_a_or_an(self, "a")
+    my.text_description(self, "Gauntlets of war.")
+    my.text_enchant(self, "+1 str, +1 def, +1 shove")
+    my.z_depth(self, my.MAP_DEPTH_OBJ)
+    my.z_prio(self, my.MAP_PRIO_BEHIND)
+
+    my.tile(self, tile=name)
+
+    my.tp_update(self)
+
+
+def init():
+    tp_init(
+        name="gauntlets_war",
+        text_name="gauntlets of war",
+        short_text_name="gauntlets of war",
+    )
+
+
+init()
