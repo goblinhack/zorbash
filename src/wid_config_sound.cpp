ff581bd7957e871a79c786fa4cf40cdd9a55841d
Neil McGill <goblinhack@gmail.com>
2022-04-13 19:37:00 +0100

    wip on ascii ui
    Former-commit-id: 337d3642496d4f5ea7fe2d04e5210292f4ae3147

--- src/wid_config_sound.cpp
+++ src/wid_config_sound.cpp
@@ -25,6 +25,7 @@ static uint8_t wid_config_sound_cancel(Widp w, int32_t x, int32_t y, uint32_t bu
   TRACE_AND_INDENT();
   CON("INF: Reload config");
   game->load_config();
+  sdl_config_update_all();
   wid_config_sound_destroy();
   game->wid_config_top_menu();
   return true;
@@ -109,19 +110,19 @@ static uint8_t wid_config_sound_key_up(Widp w, const struct SDL_Keysym *key)
   }
 
   switch (key->mod) {
-    case KMOD_LCTRL :
-    case KMOD_RCTRL :
-    default :
+    case KMOD_LCTRL:
+    case KMOD_RCTRL:
+    default:
       switch (key->sym) {
-        default :
+        default:
           {
             TRACE_AND_INDENT();
             auto c = wid_event_to_char(key);
             switch (c) {
-              case 'c' : wid_config_sound_cancel(nullptr, 0, 0, 0); return true;
-              case 's' : wid_config_sound_save(nullptr, 0, 0, 0); return true;
-              case 'b' :
-              case SDLK_ESCAPE : wid_config_sound_cancel(nullptr, 0, 0, 0); return true;
+              case 'c': wid_config_sound_cancel(nullptr, 0, 0, 0); return true;
+              case 's': wid_config_sound_save(nullptr, 0, 0, 0); return true;
+              case 'b':
+              case SDLK_ESCAPE: wid_config_sound_cancel(nullptr, 0, 0, 0); return true;
             }
           }
       }
@@ -148,12 +149,16 @@ void Game::wid_config_sound_select(void)
     wid_config_sound_destroy();
   }
 
-  auto  m     = TERM_WIDTH / 2;
-  point tl    = make_point(m - UI_WID_POPUP_WIDTH_WIDEST / 2, UI_TOPCON_VIS_HEIGHT + 2);
-  point br    = make_point(m + UI_WID_POPUP_WIDTH_WIDEST / 2, UI_ACTIONBAR_TL_Y - 2);
-  auto  width = br.x - tl.x;
+  auto box_style           = UI_WID_STYLE_HORIZ_DARK;
+  auto box_highlight_style = UI_WID_STYLE_HORIZ_LIGHT;
+  auto m                   = TERM_WIDTH / 2;
 
-  wid_config_sound_window = new WidPopup("Config sound select", tl, br, nullptr, "");
+  point tl = make_point(m - UI_WID_POPUP_WIDTH_WIDEST / 2, TERM_HEIGHT / 2 - 10);
+  point br = make_point(m + UI_WID_POPUP_WIDTH_WIDEST / 2, TERM_HEIGHT / 2 + 11);
+
+  auto width = br.x - tl.x - 2;
+
+  wid_config_sound_window = new WidPopup("Config sound select", tl, br, nullptr, "", false, false);
   {
     TRACE_AND_INDENT();
     Widp w = wid_config_sound_window->wid_popup_container;
@@ -167,7 +172,7 @@ void Game::wid_config_sound_select(void)
     auto p = wid_config_sound_window->wid_text_area->wid_text_area;
     auto w = wid_new_square_button(p, "configuration");
 
-    point tl = make_point(0, y_at);
+    point tl = make_point(1, y_at);
     point br = make_point(width, y_at + 2);
     wid_set_shape_none(w);
     wid_set_pos(w, tl, br);
@@ -180,8 +185,8 @@ void Game::wid_config_sound_select(void)
     auto p = wid_config_sound_window->wid_text_area->wid_text_area;
     auto w = wid_new_square_button(p, "Back");
 
-    point tl = make_point(0, y_at);
-    point br = make_point(5, y_at + 2);
+    point tl = make_point(1, y_at);
+    point br = make_point(6, y_at + 2);
     wid_set_style(w, UI_WID_STYLE_DARK);
     wid_set_on_mouse_up(w, wid_config_sound_back);
     wid_set_pos(w, tl, br);
@@ -192,8 +197,8 @@ void Game::wid_config_sound_select(void)
     auto p = wid_config_sound_window->wid_text_area->wid_text_area;
     auto w = wid_new_square_button(p, "Save");
 
-    point tl = make_point(width - 17, y_at);
-    point br = make_point(width - 12, y_at + 2);
+    point tl = make_point(width - 15, y_at);
+    point br = make_point(width - 10, y_at + 2);
     wid_set_style(w, UI_WID_STYLE_OK);
     wid_set_on_mouse_up(w, wid_config_sound_save);
     wid_set_pos(w, tl, br);
@@ -204,8 +209,8 @@ void Game::wid_config_sound_select(void)
     auto p = wid_config_sound_window->wid_text_area->wid_text_area;
     auto w = wid_new_square_button(p, "Cancel");
 
-    point tl = make_point(width - 10, y_at);
-    point br = make_point(width - 3, y_at + 2);
+    point tl = make_point(width - 8, y_at);
+    point br = make_point(width - 1, y_at + 2);
     wid_set_style(w, UI_WID_STYLE_HIGHLIGHTED);
     wid_set_on_mouse_up(w, wid_config_sound_cancel);
     wid_set_pos(w, tl, br);
@@ -218,8 +223,8 @@ void Game::wid_config_sound_select(void)
     auto p = wid_config_sound_window->wid_text_area->wid_text_area;
     auto w = wid_new_square_button(p, "Effects volume");
 
-    point tl = make_point(0, y_at);
-    point br = make_point(width / 2, y_at + 2);
+    point tl = make_point(1, y_at);
+    point br = make_point(width / 2, y_at);
     wid_set_shape_none(w);
     wid_set_pos(w, tl, br);
     wid_set_text_lhs(w, true);
@@ -231,8 +236,11 @@ void Game::wid_config_sound_select(void)
     auto w = wid_new_square_button(p, "Effects volume value");
 
     point tl = make_point(width / 2, y_at);
-    point br = make_point(width / 2 + 6, y_at + 2);
-    wid_set_style(w, UI_WID_STYLE_DARK);
+    point br = make_point(width / 2 + 6, y_at);
+    wid_set_mode(w, WID_MODE_OVER);
+    wid_set_style(w, box_highlight_style);
+    wid_set_mode(w, WID_MODE_NORMAL);
+    wid_set_style(w, box_style);
     wid_set_pos(w, tl, br);
     wid_set_text(w, std::to_string(game->config.sound_volume));
   }
@@ -242,8 +250,11 @@ void Game::wid_config_sound_select(void)
     auto w = wid_new_square_button(p, "Effects value +");
 
     point tl = make_point(width / 2 + 7, y_at);
-    point br = make_point(width / 2 + 9, y_at + 2);
-    wid_set_style(w, UI_WID_STYLE_DARK);
+    point br = make_point(width / 2 + 9, y_at);
+    wid_set_mode(w, WID_MODE_OVER);
+    wid_set_style(w, box_highlight_style);
+    wid_set_mode(w, WID_MODE_NORMAL);
+    wid_set_style(w, box_style);
     wid_set_pos(w, tl, br);
     wid_set_on_mouse_down(w, wid_config_sound_effects_volume_incr);
     wid_set_on_mouse_held(w, wid_config_sound_effects_volume_incr);
@@ -255,22 +266,25 @@ void Game::wid_config_sound_select(void)
     auto w = wid_new_square_button(p, "Effects value -");
 
     point tl = make_point(width / 2 + 10, y_at);
-    point br = make_point(width / 2 + 12, y_at + 2);
-    wid_set_style(w, UI_WID_STYLE_DARK);
+    point br = make_point(width / 2 + 12, y_at);
+    wid_set_mode(w, WID_MODE_OVER);
+    wid_set_style(w, box_highlight_style);
+    wid_set_mode(w, WID_MODE_NORMAL);
+    wid_set_style(w, box_style);
     wid_set_pos(w, tl, br);
     wid_set_on_mouse_down(w, wid_config_sound_effects_volume_decr);
     wid_set_on_mouse_held(w, wid_config_sound_effects_volume_decr);
     wid_set_text(w, "-");
   }
 
-  y_at += 3;
+  y_at += 1;
   {
     TRACE_AND_INDENT();
     auto p = wid_config_sound_window->wid_text_area->wid_text_area;
     auto w = wid_new_square_button(p, "Music volume");
 
-    point tl = make_point(0, y_at);
-    point br = make_point(width / 2, y_at + 2);
+    point tl = make_point(1, y_at);
+    point br = make_point(width / 2, y_at);
     wid_set_shape_none(w);
     wid_set_pos(w, tl, br);
     wid_set_text_lhs(w, true);
@@ -282,8 +296,11 @@ void Game::wid_config_sound_select(void)
     auto w = wid_new_square_button(p, "Music volume value");
 
     point tl = make_point(width / 2, y_at);
-    point br = make_point(width / 2 + 6, y_at + 2);
-    wid_set_style(w, UI_WID_STYLE_DARK);
+    point br = make_point(width / 2 + 6, y_at);
+    wid_set_mode(w, WID_MODE_OVER);
+    wid_set_style(w, box_highlight_style);
+    wid_set_mode(w, WID_MODE_NORMAL);
+    wid_set_style(w, box_style);
     wid_set_pos(w, tl, br);
     wid_set_text(w, std::to_string(game->config.music_volume));
   }
@@ -293,8 +310,11 @@ void Game::wid_config_sound_select(void)
     auto w = wid_new_square_button(p, "Music value +");
 
     point tl = make_point(width / 2 + 7, y_at);
-    point br = make_point(width / 2 + 9, y_at + 2);
-    wid_set_style(w, UI_WID_STYLE_DARK);
+    point br = make_point(width / 2 + 9, y_at);
+    wid_set_mode(w, WID_MODE_OVER);
+    wid_set_style(w, box_highlight_style);
+    wid_set_mode(w, WID_MODE_NORMAL);
+    wid_set_style(w, box_style);
     wid_set_pos(w, tl, br);
     wid_set_on_mouse_down(w, wid_config_sound_music_volume_incr);
     wid_set_on_mouse_held(w, wid_config_sound_music_volume_incr);
@@ -306,8 +326,11 @@ void Game::wid_config_sound_select(void)
     auto w = wid_new_square_button(p, "Music value -");
 
     point tl = make_point(width / 2 + 10, y_at);
-    point br = make_point(width / 2 + 12, y_at + 2);
-    wid_set_style(w, UI_WID_STYLE_DARK);
+    point br = make_point(width / 2 + 12, y_at);
+    wid_set_mode(w, WID_MODE_OVER);
+    wid_set_style(w, box_highlight_style);
+    wid_set_mode(w, WID_MODE_NORMAL);
+    wid_set_style(w, box_style);
     wid_set_pos(w, tl, br);
     wid_set_on_mouse_down(w, wid_config_sound_music_volume_decr);
     wid_set_on_mouse_held(w, wid_config_sound_music_volume_decr);
