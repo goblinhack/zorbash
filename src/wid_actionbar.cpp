a5ff41ef65bff0fc6b51313380029d99856babce
Neil McGill <goblinhack@gmail.com>
2022-04-24 08:23:13 +0100

    wip on ascii action bar

--- src/wid_actionbar.cpp
+++ src/wid_actionbar.cpp
@@ -1299,14 +1299,13 @@ void wid_actionbar_ascii_init(void)
     ui_icon_close = true;
   }
 
-  int options = 9;
+  int options = 7;
 
   if (ui_icon_collect) {
     options++;
   }
 
   if (ui_icon_close) {
-    options++;
     options++;
   }
 
@@ -1326,181 +1325,173 @@ void wid_actionbar_ascii_init(void)
     }
   }
 
-  int option_width = 4;
-  int w            = options * option_width;
-  int left_half    = w / 2;
-  int right_half   = w - left_half;
+  int option_width  = 9;
+  int option_height = 1;
+
+  int w          = options * (option_width + 1);
+  int left_half  = w / 2;
+  int right_half = w - left_half;
 
   {
-    point tl = make_point(TERM_WIDTH / 2 - left_half, TERM_HEIGHT - (option_width + 1));
+    point tl = make_point(TERM_WIDTH / 2 - left_half, TERM_HEIGHT - (option_height + 1));
     point br = make_point(TERM_WIDTH / 2 + right_half - 1, TERM_HEIGHT - 2);
 
     wid_actionbar = wid_new_square_window("wid actionbar");
     wid_set_ignore_scroll_events(wid_actionbar, true);
     wid_set_pos(wid_actionbar, tl, br);
-    wid_set_style(wid_actionbar, UI_WID_STYLE_DARK);
+    wid_set_shape_none(wid_actionbar);
     wid_lower(wid_actionbar);
   }
 
   int x_at = 0;
 
-  if (ui_icon_close) {
-    auto  w  = wid_new_square_button(wid_actionbar, "wid actionbar close");
-    point tl = make_point(x_at, 0);
-    point br = make_point(x_at + option_width - 1, option_width - 1);
-    wid_set_pos(w, tl, br);
-    wid_set_on_mouse_up(w, wid_actionbar_close);
-    wid_set_on_mouse_over_begin(w, wid_actionbar_close_over_b);
-    wid_set_on_mouse_over_end(w, wid_actionbar_close_over_e);
-    x_at += option_width;
-  }
-
   {
     auto  w  = wid_new_square_button(wid_actionbar, "wid actionbar quit");
     point tl = make_point(x_at, 0);
-    point br = make_point(x_at + option_width - 1, option_width - 1);
+    point br = make_point(x_at + option_width - 1, option_height - 1);
     wid_set_pos(w, tl, br);
     wid_set_on_mouse_up(w, wid_actionbar_quit);
     wid_set_on_mouse_over_begin(w, wid_actionbar_quit_over_b);
     wid_set_on_mouse_over_end(w, wid_actionbar_quit_over_e);
-    x_at += option_width;
+    wid_set_text(w, "Quit");
+    wid_set_style(w, UI_WID_STYLE_NORMAL);
+    x_at += option_width + 1;
   }
 
   {
     auto  w  = wid_new_square_button(wid_actionbar, "wid actionbar config");
     point tl = make_point(x_at, 0);
-    point br = make_point(x_at + option_width - 1, option_width - 1);
+    point br = make_point(x_at + option_width - 1, option_height - 1);
     wid_set_pos(w, tl, br);
     wid_set_on_mouse_up(w, wid_actionbar_configure);
     wid_set_on_mouse_over_begin(w, wid_actionbar_configure_over_b);
     wid_set_on_mouse_over_end(w, wid_actionbar_configure_over_e);
-    x_at += option_width;
+    wid_set_text(w, "Keys");
+    wid_set_style(w, UI_WID_STYLE_NORMAL);
+    x_at += option_width + 1;
   }
 
   {
     auto  w  = wid_new_square_button(wid_actionbar, "wid actionbar robot");
     point tl = make_point(x_at, 0);
-    point br = make_point(x_at + option_width - 1, option_width - 1);
+    point br = make_point(x_at + option_width - 1, option_height - 1);
     wid_set_pos(w, tl, br);
     wid_set_on_mouse_up(w, wid_actionbar_robot);
     wid_set_on_mouse_over_begin(w, wid_actionbar_robot_over_b);
     wid_set_on_mouse_over_end(w, wid_actionbar_robot_over_e);
+    wid_set_text(w, "Robot On");
+    wid_set_style(w, UI_WID_STYLE_NORMAL);
     if (game->robot_mode) {
       wid_set_on_tick(w, wid_actionbar_ai_tick);
+      wid_set_text(w, "Robot off");
+      wid_set_style(w, UI_WID_STYLE_RED);
     }
-    wid_set_style(w, UI_WID_STYLE_RED);
-    x_at += option_width;
+    x_at += option_width + 1;
   }
 
   if (! game->request_ascend && add_ascend) {
     auto  w  = wid_new_square_button(wid_actionbar, "wid actionbar ascend");
     point tl = make_point(x_at, 0);
-    point br = make_point(x_at + option_width - 1, option_width - 1);
+    point br = make_point(x_at + option_width - 1, option_height - 1);
     wid_set_pos(w, tl, br);
     wid_set_on_mouse_up(w, wid_actionbar_ascend);
     wid_set_on_mouse_over_begin(w, wid_actionbar_ascend_over_b);
     wid_set_on_mouse_over_end(w, wid_actionbar_ascend_over_e);
-    x_at += option_width;
+    wid_set_text(w, "Ascend");
+    wid_set_style(w, UI_WID_STYLE_NORMAL);
+    x_at += option_width + 1;
   }
 
   if (! game->request_descend && add_descend) {
     auto  w  = wid_new_square_button(wid_actionbar, "wid actionbar descend");
     point tl = make_point(x_at, 0);
-    point br = make_point(x_at + option_width - 1, option_width - 1);
+    point br = make_point(x_at + option_width - 1, option_height - 1);
     wid_set_pos(w, tl, br);
     wid_set_on_mouse_up(w, wid_actionbar_descend);
     wid_set_on_mouse_over_begin(w, wid_actionbar_descend_over_b);
     wid_set_on_mouse_over_end(w, wid_actionbar_descend_over_e);
-    x_at += option_width;
+    wid_set_text(w, "Descend");
+    wid_set_style(w, UI_WID_STYLE_NORMAL);
+    x_at += option_width + 1;
   }
 
   {
     auto  w  = wid_new_square_button(wid_actionbar, "wid actionbar save");
     point tl = make_point(x_at, 0);
-    point br = make_point(x_at + option_width - 1, option_width - 1);
+    point br = make_point(x_at + option_width - 1, option_height - 1);
     wid_set_pos(w, tl, br);
     wid_set_on_mouse_up(w, wid_actionbar_save);
     wid_set_on_mouse_over_begin(w, wid_actionbar_save_over_b);
     wid_set_on_mouse_over_end(w, wid_actionbar_save_over_e);
-    x_at += option_width;
+    wid_set_text(w, "Save");
+    wid_set_style(w, UI_WID_STYLE_NORMAL);
+    x_at += option_width + 1;
   }
 
   {
     auto  w  = wid_new_square_button(wid_actionbar, "wid actionbar load");
     point tl = make_point(x_at, 0);
-    point br = make_point(x_at + option_width - 1, option_width - 1);
+    point br = make_point(x_at + option_width - 1, option_height - 1);
     wid_set_pos(w, tl, br);
     wid_set_on_mouse_up(w, wid_actionbar_load);
     wid_set_on_mouse_over_begin(w, wid_actionbar_load_over_b);
     wid_set_on_mouse_over_end(w, wid_actionbar_load_over_e);
-    x_at += option_width;
-  }
-
-  {
-    auto  w  = wid_new_square_button(wid_actionbar, "wid actionbar zoom_in");
-    point tl = make_point(x_at, 0);
-    point br = make_point(x_at + option_width - 1, option_width - 1);
-    wid_set_pos(w, tl, br);
-    wid_set_on_mouse_up(w, wid_actionbar_zoom_in);
-    wid_set_on_mouse_over_begin(w, wid_actionbar_zoom_in_over_b);
-    wid_set_on_mouse_over_end(w, wid_actionbar_zoom_in_over_e);
-    x_at += option_width;
-  }
-
-  {
-    auto  w  = wid_new_square_button(wid_actionbar, "wid actionbar zoom_out");
-    point tl = make_point(x_at, 0);
-    point br = make_point(x_at + option_width - 1, option_width - 1);
-    wid_set_pos(w, tl, br);
-    wid_set_on_mouse_up(w, wid_actionbar_zoom_out);
-    wid_set_on_mouse_over_begin(w, wid_actionbar_zoom_out_over_b);
-    wid_set_on_mouse_over_end(w, wid_actionbar_zoom_out_over_e);
-    x_at += option_width;
+    wid_set_text(w, "Load");
+    wid_set_style(w, UI_WID_STYLE_NORMAL);
+    x_at += option_width + 1;
   }
 
   {
     auto  w  = wid_new_square_button(wid_actionbar, "wid actionbar inventory");
     point tl = make_point(x_at, 0);
-    point br = make_point(x_at + option_width - 1, option_width - 1);
+    point br = make_point(x_at + option_width - 1, option_height - 1);
     wid_set_pos(w, tl, br);
     wid_set_on_mouse_up(w, wid_actionbar_inventory);
     wid_set_on_mouse_over_begin(w, wid_actionbar_inventory_over_b);
     wid_set_on_mouse_over_end(w, wid_actionbar_inventory_over_e);
-    x_at += option_width;
+    wid_set_text(w, "Inventory");
+    wid_set_style(w, UI_WID_STYLE_NORMAL);
+    x_at += option_width + 1;
   }
 
   {
     auto  w  = wid_new_square_button(wid_actionbar, "wid actionbar wait");
     point tl = make_point(x_at, 0);
-    point br = make_point(x_at + option_width - 1, option_width - 1);
+    point br = make_point(x_at + option_width - 1, option_height - 1);
     wid_set_pos(w, tl, br);
     wid_set_on_mouse_down(w, wid_actionbar_wait);
     wid_set_on_mouse_held(w, wid_actionbar_repeat_wait);
     wid_set_on_mouse_over_begin(w, wid_actionbar_wait_over_b);
     wid_set_on_mouse_over_end(w, wid_actionbar_wait_over_e);
-    x_at += option_width;
+    wid_set_text(w, "Wait");
+    wid_set_style(w, UI_WID_STYLE_NORMAL);
+    x_at += option_width + 1;
   }
 
   if (ui_icon_collect) {
     auto  w  = wid_new_square_button(wid_actionbar, "wid actionbar collect");
     point tl = make_point(x_at, 0);
-    point br = make_point(x_at + option_width - 1, option_width - 1);
+    point br = make_point(x_at + option_width - 1, option_height - 1);
     wid_set_pos(w, tl, br);
     wid_set_on_mouse_up(w, wid_actionbar_collect);
     wid_set_on_mouse_over_begin(w, wid_actionbar_collect_over_b);
     wid_set_on_mouse_over_end(w, wid_actionbar_collect_over_e);
-    x_at += option_width;
+    wid_set_text(w, "Collect");
+    wid_set_style(w, UI_WID_STYLE_NORMAL);
+    x_at += option_width + 1;
   }
 
   if (ui_icon_close) {
     auto  w  = wid_new_square_button(wid_actionbar, "wid actionbar close");
     point tl = make_point(x_at, 0);
-    point br = make_point(x_at + option_width - 1, option_width - 1);
+    point br = make_point(x_at + option_width - 1, option_height - 1);
     wid_set_pos(w, tl, br);
     wid_set_on_mouse_up(w, wid_actionbar_close);
     wid_set_on_mouse_over_begin(w, wid_actionbar_close_over_b);
     wid_set_on_mouse_over_end(w, wid_actionbar_close_over_e);
-    x_at += option_width;
+    wid_set_text(w, "Close");
+    wid_set_style(w, UI_WID_STYLE_NORMAL);
+    x_at += option_width + 1;
   }
   wid_update(wid_actionbar);
 }
