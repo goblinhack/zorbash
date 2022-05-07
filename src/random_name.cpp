ab9f2efbf4b33c9ab49e679225249265daa17e21
Neil McGill <goblinhack@gmail.com>
2022-05-07 19:35:25 +0100

    cross platform hash

--- src/random_name.cpp
+++ src/random_name.cpp
@@ -23,4 +23,23 @@ std::string random_name(int len)
   }
 }
 
-uint32_t string_to_hash(const std::string &str) { return static_cast< uint32_t >(std::hash< std::string > {}(str)); }
+// https://stackoverflow.com/questions/7666509/hash-function-for-string
+static unsigned long hash(const char *s)
+{
+  unsigned long hash = 5381;
+  char          c;
+
+  while ((c = *s++))
+    hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+  return hash;
+}
+
+uint32_t string_to_hash(const std::string &str)
+{
+#ifdef FANCY_BUT_GIVES_DIFFERENT_RESULTS_ON_DIFFERENT_PLATFORMS
+  return static_cast< uint32_t >(std::hash< std::string > {}(str));
+#else
+  return hash(str.c_str());
+#endif
+}
