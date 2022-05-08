7c1cb20f5bdc896dc0163a5630af0de7065e985d
Neil McGill <goblinhack@gmail.com>
2022-05-08 16:21:09 +0100

    all asci chars assigned
    Former-commit-id: 3479e1331889559bbecc239f12385129a7863964

--- python/things/gauntlets/wooden_cross.py
+++ python/things/gauntlets/wooden_cross.py
@@ -50,7 +50,7 @@ def tp_init(name, text_name, short_text_name):
     my.z_prio(self, my.MAP_PRIO_BEHIND)
 
     my.tile(self,
-            ascii_fg_char="X", ascii_bg_col_name="", ascii_fg_col_name="white",
+            ascii_fg_char="cross", ascii_bg_col_name="", ascii_fg_col_name="brown",
             tile=name, delay_ms=100)
 
     my.tp_update(self)
