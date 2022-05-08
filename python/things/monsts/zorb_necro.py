330f6f7f91ef3f49faf2055cb59379e74029c84a
Neil McGill <goblinhack@gmail.com>
2022-05-08 16:43:52 +0100

    more monst ascii assigned
    Former-commit-id: 5e8570922295406a1950ba04136fdf18b0daa37d

--- python/things/monsts/zorb_necro.py
+++ python/things/monsts/zorb_necro.py
@@ -72,44 +72,44 @@ def tp_init(name, text_name):
 
     delay = 300
     my.tile(self,
-            ascii_fg_char="m", ascii_bg_col_name="", ascii_fg_col_name="white",
+            ascii_fg_char="z", ascii_bg_col_name="", ascii_fg_col_name="green",
             tile=name + ".1", delay_ms=delay, frame=1)
     my.tile(self,
-            ascii_fg_char="m", ascii_bg_col_name="", ascii_fg_col_name="white",
+            ascii_fg_char="z", ascii_bg_col_name="", ascii_fg_col_name="green",
             tile=name + ".2", delay_ms=delay, frame=2)
     my.tile(self,
-            ascii_fg_char="m", ascii_bg_col_name="", ascii_fg_col_name="white",
+            ascii_fg_char="z", ascii_bg_col_name="", ascii_fg_col_name="green",
             tile=name + ".3", delay_ms=delay, frame=3)
     my.tile(self,
-            ascii_fg_char="m", ascii_bg_col_name="", ascii_fg_col_name="white",
+            ascii_fg_char="z", ascii_bg_col_name="", ascii_fg_col_name="green",
             tile=name + ".4", delay_ms=delay, frame=4)
     my.tile(self,
-            ascii_fg_char="m", ascii_bg_col_name="", ascii_fg_col_name="white",
+            ascii_fg_char="z", ascii_bg_col_name="", ascii_fg_col_name="green",
             tile=name + ".5", delay_ms=delay, frame=5)
     my.tile(self,
-            ascii_fg_char="m", ascii_bg_col_name="", ascii_fg_col_name="white",
+            ascii_fg_char="z", ascii_bg_col_name="", ascii_fg_col_name="green",
             tile=name + ".6", delay_ms=delay, frame=6)
     my.tile(self,
-            ascii_fg_char="m", ascii_bg_col_name="", ascii_fg_col_name="white",
+            ascii_fg_char="z", ascii_bg_col_name="", ascii_fg_col_name="green",
             tile=name + ".1", is_sleeping=True, delay_ms=delay, frame=1)
     my.tile(self,
-            ascii_fg_char="m", ascii_bg_col_name="", ascii_fg_col_name="white",
+            ascii_fg_char="z", ascii_bg_col_name="", ascii_fg_col_name="green",
             tile=name + ".2", is_sleeping=True, delay_ms=delay, frame=2)
 
     my.tile(self,
-            ascii_fg_char="m", ascii_bg_col_name="", ascii_fg_col_name="white",
+            ascii_fg_char="z", ascii_bg_col_name="", ascii_fg_col_name="green",
             tile=name + ".dead.1", is_dead=True, delay_ms=delay)
     my.tile(self,
-            ascii_fg_char="m", ascii_bg_col_name="", ascii_fg_col_name="white",
+            ascii_fg_char="z", ascii_bg_col_name="", ascii_fg_col_name="green",
             tile=name + ".dead.2", is_dead=True, delay_ms=delay)
     my.tile(self,
-            ascii_fg_char="m", ascii_bg_col_name="", ascii_fg_col_name="white",
+            ascii_fg_char="z", ascii_bg_col_name="", ascii_fg_col_name="green",
             tile=name + ".dead.3", is_dead=True, delay_ms=delay)
     my.tile(self,
-            ascii_fg_char="m", ascii_bg_col_name="", ascii_fg_col_name="white",
+            ascii_fg_char="z", ascii_bg_col_name="", ascii_fg_col_name="green",
             tile=name + ".dead.4", is_dead=True, delay_ms=delay)
     my.tile(self,
-            ascii_fg_char="m", ascii_bg_col_name="", ascii_fg_col_name="white",
+            ascii_fg_char="z", ascii_bg_col_name="", ascii_fg_col_name="green",
             tile=name + ".dead.5", is_dead=True, is_end_of_anim=True)
 
     my.tp_update(self)
