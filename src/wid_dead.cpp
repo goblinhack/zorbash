672981ceb9cb0759b2317aa93df9b6a7cbbb9efc
Neil McGill <goblinhack@gmail.com>
2022-06-18 20:26:40 +0100

    add ascii gravestone
    Former-commit-id: dbcbfff01c704dc5e8d4cb2e5a8d582d4f0590e2

--- src/wid_dead.cpp
+++ src/wid_dead.cpp
@@ -72,7 +72,7 @@ static uint8_t wid_dead_key_down(Widp w, const struct SDL_Keysym *key)
     return false;
   }
 
-  return true;
+  return false;
 }
 
 static uint8_t wid_dead_mouse_up(Widp w, int32_t x, int32_t y, uint32_t button)
@@ -113,7 +113,11 @@ void Game::wid_dead_select(const char *reason)
   point br    = make_point(UI_WID_POPUP_WIDTH_WIDE - 1, TERM_HEIGHT - 1);
   auto  width = br.x - tl.x - 1;
 
-  wid_dead_window = new WidPopup("Game dead", tl, br, nullptr, "ui_dead", false, false);
+  if (g_opt_ascii) {
+    wid_dead_window = new WidPopup("Game dead", tl, br, nullptr, "", false, false);
+  } else {
+    wid_dead_window = new WidPopup("Game dead", tl, br, nullptr, "ui_dead", false, false);
+  }
 
   wid_set_on_key_up(wid_dead_window->wid_popup_container, wid_dead_key_up);
   wid_set_on_key_down(wid_dead_window->wid_popup_container, wid_dead_key_down);
@@ -122,37 +126,103 @@ void Game::wid_dead_select(const char *reason)
   // Want console to be able to be on top
   // wid_set_do_not_lower(wid_dead_window->wid_popup_container, true);
 
-  wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
-  wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
-  wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
-  wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
+  if (g_opt_ascii) {
+    wid_dead_window->log("`     .------------.     `");
+    wid_dead_window->log("`    /     Rest     \\    `");
+    wid_dead_window->log("`   /       In       \\   `");
+    wid_dead_window->log("`  /       Peace      \\  `");
+    wid_dead_window->log("`  |                  |  `");
+    wid_dead_window->log("`  |                  |  `");
 
-  if (! g_opt_ascii) {
-    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
-    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
-    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
-    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
-    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
-    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
-    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
-    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
-    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
-    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
-    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
-    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
-    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
-    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
-    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
-    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
-  }
+    //
+    // Center the player name
+    //
+    {
+      std::string text    = capitalise(level->player->title());
+      int         pad     = UI_RIGHTBAR_WIDTH - text.size();
+      int         lhs_pad = pad / 2;
+      int         rhs_pad = pad - lhs_pad;
 
-  if (game->robot_mode) {
-    wid_dead_window->log("Deactivated...");
+      std::string grave_name = "`   |";
+      while (lhs_pad--) {
+        grave_name += " ";
+      }
+      grave_name += text;
+      while (rhs_pad--) {
+        grave_name += " ";
+      }
+      grave_name += "|   `";
+
+      wid_dead_window->log(grave_name);
+    }
+
+    if (game->robot_mode) {
+      wid_dead_window->log("`  |   Deactivated    |  `");
+    } else {
+      wid_dead_window->log("`  |      Killed      |  `");
+    }
+
+    //
+    // Center the death reason
+    //
+    {
+      auto d = split(capitalise(reason), UI_RIGHTBAR_WIDTH);
+
+      for (const auto &text : *d) {
+        int pad     = UI_RIGHTBAR_WIDTH - text.size();
+        int lhs_pad = pad / 2;
+        int rhs_pad = pad - lhs_pad;
+
+        std::string death_reason = "`   |";
+        while (lhs_pad--) {
+          death_reason += " ";
+        }
+        death_reason += text;
+        while (rhs_pad--) {
+          death_reason += " ";
+        }
+        death_reason += "|   `";
+
+        wid_dead_window->log(death_reason);
+      }
+    }
+
+    wid_dead_window->log("`  |                  |  `");
+    wid_dead_window->log(
+        "`%%fg=yellow$*%%fg=reset$ |     %%fg=yellow$*%%fg=reset$        %%fg=yellow$*%%fg=reset$   |  `");
+    wid_dead_window->log(
+        "%%fg=green$_|'''%%fg=reset$__%%fg=green$'\"|%%fg=reset$__%%fg=green$.'\"'.\"|'..%%fg=reset$|%%fg=green$___");
   } else {
-    wid_dead_window->log("Killed...");
+    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
+    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
+    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
+    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
+    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
+    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
+    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
+    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
+    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
+    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
+    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
+    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
+    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
+    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
+    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
+    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
+    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
+    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
+    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
+    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
+    wid_dead_window->log(capitalise(level->player->title()));
+    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
+    if (game->robot_mode) {
+      wid_dead_window->log("Deactivated...");
+    } else {
+      wid_dead_window->log("Killed...");
+    }
+    wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
+    wid_dead_window->log(reason);
   }
-  wid_dead_window->log(UI_LOGGING_EMPTY_LINE);
-  wid_dead_window->log(reason);
 
   game->robot_mode           = false;
   game->robot_mode_requested = false;
@@ -193,20 +263,23 @@ void Game::wid_dead_select(const char *reason)
     }
   }
 
-  {
-    TRACE_AND_INDENT();
-    auto p = wid_dead_window->wid_text_area->wid_text_area;
-    auto w = wid_new_square_button(p, "dead");
+  TRACE_AND_INDENT();
+  auto p = wid_dead_window->wid_text_area->wid_text_area;
+  auto w = wid_new_square_button(p, "dead");
 
+  if (g_opt_ascii) {
+    point tl = make_point(9, h - 7);
+    point br = make_point(width - 11, h - 5);
+    wid_set_pos(w, tl, br);
+  } else {
     point tl = make_point(9, h - 8);
     point br = make_point(width - 11, h - 6);
-
-    wid_set_style(w, UI_WID_STYLE_RED);
-    wid_set_on_mouse_up(w, wid_dead_mouse_up);
-
     wid_set_pos(w, tl, br);
-    wid_set_text(w, "Continue");
   }
+
+  wid_set_style(w, UI_WID_STYLE_RED);
+  wid_set_on_mouse_up(w, wid_dead_mouse_up);
+  wid_set_text(w, "Continue");
 
   wid_update(wid_dead_window->wid_text_area->wid_text_area);
 }
