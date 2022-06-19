c59e5cf8891b26346431fce797957aa6e1b86b0d
Neil McGill <goblinhack@gmail.com>
2022-06-19 15:30:18 +0100

    wip on ascii inventory
    Former-commit-id: 00c30633d66280f82681fa403ba7f50f843faf4b

--- python/things/dungeon/pillar.py
+++ python/things/dungeon/pillar.py
@@ -15,6 +15,7 @@ def tp_init(name, text_long_name, tiles=[]):
     my.is_able_to_fall(self, True)
     my.is_always_hit(self, True)
     my.is_cursor_path_blocker(self, True)
+    my.is_described_when_hovering_over(self, True)
     my.is_floor_deco(self, True)
     my.is_interesting(self, True)
     my.is_jump_blocker(self, True)
