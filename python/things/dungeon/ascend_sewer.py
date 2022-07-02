f4d19d94571c8dfef65405e13a5d2231c3147f48
Neil McGill <goblinhack@gmail.com>
2022-07-02 21:32:28 +0100

    brighter sewer
    Former-commit-id: f064fd925926b603db7ac5a84d6f4b2a5f9008d1

--- python/things/dungeon/ascend_sewer.py
+++ python/things/dungeon/ascend_sewer.py
@@ -37,7 +37,7 @@ def tp_init(name, tiles=[], left1_tiles=[]):
     delay = 1000
     for t in tiles:
         my.tile(self,
-                ascii_fg_char="<", ascii_bg_col_name="", ascii_fg_col_name="darkgreen",
+                ascii_fg_char="<", ascii_bg_col_name="", ascii_fg_col_name="green",
                 tile=t, delay_ms=delay)
 
     my.tp_update(self)
