222d0bf6f07ade46df364f2342ffb266e4bcdcc8
Neil McGill <goblinhack@gmail.com>
2022-10-09 15:50:42 +0100

    fix typo

--- python/things/debuffs/debuff_hungry.py
+++ python/things/debuffs/debuff_hungry.py
@@ -15,7 +15,7 @@ def tp_init(name, text_long_name, text_short_name):
 
     # start sort marker
     my.gfx_ascii_shown(self, True)
-    my.is_buff(self, True)
+    my.is_debuff(self, True)
     my.is_loggable(self, True)
     my.on_owner_add_do(self, "me.on_owner_add()")
     my.text_long_description(self, "You are hungry and may not attack, defend or jump as well as you did.")
