f1aa27bdb0e6a2911a4d7fffc4441a58d4e9b0c4
Neil McGill <goblinhack@gmail.com>
2022-10-29 15:47:40 +0100

    workaround for clang for source_location

--- src/my_source_loc.hpp
+++ src/my_source_loc.hpp
@@ -2,10 +2,27 @@
 #ifndef _MY_SOURCE_LOC_HPP_
 #define _MY_SOURCE_LOC_HPP_
 
-#include <source_location>
+#define HAVE_WORKING_SOURCE_LOCATION_HEADER
 
+#ifdef __clang__
+// Seems to be missing for clang. Including <experimental/source_location> did not help
+#undef HAVE_WORKING_SOURCE_LOCATION_HEADER
+#endif
+
+#ifdef __APPLE__
+// Missing with macports
+#undef HAVE_WORKING_SOURCE_LOCATION_HEADER
+#endif
+
+#ifdef HAVE_WORKING_SOURCE_LOCATION_HEADER
+#include <source_location>
 #define SRC_FILE_NAME (std::source_location::current().file_name())
 #define SRC_FUNC_NAME (std::source_location::current().function_name())
 #define SRC_LINE_NUM  (std::source_location::current().line())
+#else
+#define SRC_FILE_NAME __FILE__
+#define SRC_FUNC_NAME __PRETTY_FUNCTION__
+#define SRC_LINE_NUM  __LINE__
+#endif
 
 #endif // _MY_SOURCE_LOC_HPP_
