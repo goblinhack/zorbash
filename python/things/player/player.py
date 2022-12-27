ef38f15d325609d7e203996f1ec23e39a3fa6b8d
Neil McGill <goblinhack@gmail.com>
2022-12-27 16:42:53 +0000

    square outlines for weapons

--- python/things/player/player.py
+++ python/things/player/player.py
@@ -371,6 +371,7 @@ def tp_init(name, text_long_name, text_short_name, title):
     my.gfx_pixelart_shadow(self, True)
     my.gfx_pixelart_shadow_short(self, True)
     my.gfx_pixelart_show_outlined(self, True)
+    my.gfx_pixelart_show_square_outlined(self, True)
     my.gfx_pixelart_submergable(self, True)
     my.gfx_pixelart_wobbles_when_hit(self, True)
     my.health_initial_dice(self, "100")
