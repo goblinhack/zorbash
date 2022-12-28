586f337d761b86b9f632f241b353bf3e4cfef47f
Neil McGill <goblinhack@gmail.com>
2022-12-28 11:18:02 +0000

    pics

--- src/game_player.cpp
+++ src/game_player.cpp
@@ -251,7 +251,7 @@ void Game::place_player(void)
         t->carry(w);
         t->enchant_without_stone(w);
       }
-      if (0) {
+      if (1) {
         level->thing_new("water", point(x + 1, y));
         level->thing_new("deep_water", point(x + 2, y));
         level->thing_new("deep_water", point(x + 3, y));
