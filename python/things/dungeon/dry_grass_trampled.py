259f10f277112689db81784fa97590aa749c9f28
Neil McGill <goblinhack@gmail.com>
2023-03-09 19:29:06 +0000

    no shadows for trampled grass

--- python/things/dungeon/dry_grass_trampled.py
+++ python/things/dungeon/dry_grass_trampled.py
@@ -16,8 +16,6 @@ def tp_init(name, tiles=[]):
     my.gfx_ascii_shown_in_bg(self, True)
     my.gfx_ascii_shown(self, True)
     my.gfx_pixelart_reflection(self, True)
-    my.gfx_pixelart_shadow(self, True)
-    my.gfx_pixelart_shadow_very_short(self, True)
     my.gfx_pixelart_shown_in_bg(self, True)
     my.gfx_pixelart_submergable(self, True)
     my.is_able_to_fall(self, True)
