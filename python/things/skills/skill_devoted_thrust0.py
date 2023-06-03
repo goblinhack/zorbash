5c29541a2770d89d367c83426a9c6b8deba3e054
Neil McGill <goblinhack@gmail.com>
2023-06-03 15:53:07 +0100

    common skill parts for golden health

--- python/things/skills/skill_devoted_thrust0.py
+++ python/things/skills/skill_devoted_thrust0.py
@@ -10,12 +10,12 @@ def tp_init(self):
     my.on_tick_when_activated_do(self, "me.on_tick_when_activated()")
     my.on_use_do(self, "me.on_use()")
     my.skill_base_name(self, "skill_devoted_thrust")
-    my.tick_prio(self, my.MAP_TICK_PRIO_NORMAL)
-    my.z_prio(self, my.MAP_Z_PRIO_ALWAYS_BEHIND)
-    # end sort marker
-    my.tile(self, tile="skill_devoted_thrust_activated")
-    my.tile(self, tile="skill_devoted_thrust")
-    my.text_description_long(self, "With devotion to the god, gods, or whatever it is that you worship, you willingly sacrifice life energy in the pursuit of maximal damage.")
     my.text_description_long2(self, "The Devoted Thrust skill directs your stamina into sheer vanquishing power.")
     my.text_description_long3(self, "Be warned that at high levels of this skill, you will become drained very quickly and may render yourself unable to move or jump, or escape a trap.")
+    my.text_description_long(self, "With devotion to the god, gods, or whatever it is that you worship, you willingly sacrifice life energy in the pursuit of maximal damage.")
     my.text_description_short(self, "Devoted thrust skill.")
+    my.tick_prio(self, my.MAP_TICK_PRIO_NORMAL)
+    my.tile(self, tile="skill_devoted_thrust")
+    my.tile(self, tile="skill_devoted_thrust_activated")
+    my.z_prio(self, my.MAP_Z_PRIO_ALWAYS_BEHIND)
+    # end sort marker
--- python/things/skills/skill_gold_to_health4.py
+++ python/things/skills/skill_gold_to_health4.py
@@ -1,5 +1,6 @@
 import my
 import tp
+import skill_gold_to_health0
 
 self = None
 
@@ -28,25 +29,15 @@ def tp_init(name, text_long_name, text_short_name):
     global self
     self = tp.Tp(name, text_long_name, text_short_name)
     # begin sort marker
-    my.gfx_ascii_shown(self, True)
-    my.is_loggable(self, True)
-    my.is_skill(self, True)
-    my.is_tickable(self, True)
-    my.on_tick_when_activated_do(self, "me.on_tick_when_activated()")
-    my.skill_base_name(self, "skill_gold_to_health")
-    my.skill_replaces(self, "skill_gold_to_health3")
     my.stat_con_bonus(self, 2)
     my.stat_dex_bonus(self, -4)
-    my.text_description_long2(self, "When in use, your skin glows radiantly. However you will also feel sluggish and less dextrous.")
-    my.text_description_long3(self, "Conversion rate is 10 gold per turn to 5 health.")
-    my.text_description_long(self, "With this skill you are able to trans-mutate your hard earned gold into life giving health.")
-    my.text_description_short(self, "Golden health skill.")
+    my.text_description_long3(self, "Conversion rate is 8 gold per turn to 4 health.")
     my.text_description_very_short(self, "GldToHp 4")
+    my.skill_replaces(self, "skill_gold_to_health3")
     my.tick_prio(self, my.MAP_TICK_PRIO_NORMAL)
     my.z_prio(self, my.MAP_Z_PRIO_ALWAYS_BEHIND)
     # end sort marker
-    my.tile(self, tile="skill_gold_to_health_activated")
-    my.tile(self, tile="skill_gold_to_health")
+    skill_gold_to_health0.tp_init(self)
     my.tp_update(self)
 
 
