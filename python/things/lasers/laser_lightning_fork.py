b5c3885b441b3fcdf99456e442ad3ccad05cc6c5
Neil McGill <goblinhack@gmail.com>
2023-07-07 15:48:10 +0100

    missing files

---
+++ python/things/lasers/laser_lightning_fork.py
@@ -0,0 +1,64 @@
+import my
+import tp
+
+
+def on_born(me, x, y):
+    selection_x, selection_y = my.thing_coords_get(me)
+    for it in my.level_get_all(me, selection_x, selection_y):
+        if my.thing_possible_to_attack(me, it):
+            my.thing_hit(0, me, it)
+    my.spawn_at_my_position(me, "explosion_fire")
+    my.thing_sound_play_channel(me, my.CHANNEL_WEAPON, "lightning_b")
+
+
+#
+# This is an internal only object to fire lasers from monsters
+#
+def tp_init(name, text_long_name, text_short_name):
+    self = tp.Tp(name, text_long_name, text_short_name)
+    # begin sort marker
+    my.dmg_chance_d1000_lightning(self, 0, 1000)
+    my.dmg_lightning_dice(self, "1d5+4")
+    my.gfx_ascii_shown(self, True)
+    my.is_able_to_attack_owner(self, True)
+    my.is_laser(self, True)
+    my.is_lightning(self, True)
+    my.is_loggable(self, True)
+    my.is_no_tile(self, True)
+    my.is_usable(self, True)
+    my.on_born_do(self, "me.on_born()")
+    my.text_a_or_an(self, "a")
+    my.tick_prio(self, my.MAP_TICK_PRIO_VERY_HIGH)
+    my.z_depth(self, my.MAP_DEPTH_OBJ)
+    my.z_prio(self, my.MAP_Z_PRIO_BEHIND)
+    # end sort marker
+
+    my.tile(self, ascii_fg_char="block", ascii_fg_col_name="cyan", ascii_fg_alpha=255, tile="nothing")
+    my.tile(self, ascii_fg_char="block", ascii_fg_col_name="white", ascii_fg_alpha=255, tile="nothing")
+    my.tile(self, ascii_fg_char="block", ascii_fg_col_name="cyan", ascii_fg_alpha=255, tile="nothing")
+    my.tile(self, ascii_fg_char="block", ascii_fg_col_name="white", ascii_fg_alpha=255, tile="nothing")
+    my.tile(self, ascii_fg_char="block", ascii_fg_col_name="cyan", ascii_fg_alpha=255, tile="nothing")
+    my.tile(self, ascii_fg_char="block", ascii_fg_col_name="white", ascii_fg_alpha=200, tile="nothing")
+    my.tile(self, ascii_fg_char="block", ascii_fg_col_name="cyan", ascii_fg_alpha=200, tile="nothing")
+    my.tile(self, ascii_fg_char="block", ascii_fg_col_name="white", ascii_fg_alpha=200, tile="nothing")
+    my.tile(self, ascii_fg_char="block", ascii_fg_col_name="cyan", ascii_fg_alpha=200, tile="nothing")
+    my.tile(self, ascii_fg_char="block", ascii_fg_col_name="white", ascii_fg_alpha=100, tile="nothing")
+    my.tile(self, ascii_fg_char="block", ascii_fg_col_name="cyan", ascii_fg_alpha=100, tile="nothing")
+    my.tile(self, ascii_fg_char="block", ascii_fg_col_name="white", ascii_fg_alpha=100, tile="nothing")
+    my.tile(self, ascii_fg_char="block", ascii_fg_col_name="cyan", ascii_fg_alpha=100, tile="nothing")
+    my.tile(self, ascii_fg_char="block", ascii_fg_col_name="white", ascii_fg_alpha=50, tile="nothing")
+    my.tile(self, ascii_fg_char="block", ascii_fg_col_name="cyan", ascii_fg_alpha=50, tile="nothing")
+    my.tile(self, ascii_fg_char="block", ascii_fg_col_name="white", ascii_fg_alpha=50, tile="nothing")
+
+    my.tp_update(self)
+
+
+def init():
+    tp_init(
+        name="laser_lightning_fork",
+        text_long_name="fork lightning",
+        text_short_name="fork lightning",
+    )
+
+
+init()
