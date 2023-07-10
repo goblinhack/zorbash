a66ebd753f823e1fdcf0a8dec6db65ef5807537a
Neil McGill <goblinhack@gmail.com>
2023-07-10 20:56:39 +0100

    bug: level hang when dead things fall into the void

--- src/thing_the_void.cpp
+++ src/thing_the_void.cpp
@@ -6,10 +6,13 @@
 
 void Thing::fall_into_the_void(void)
 {
+  dbg("Try to fall into the void");
+
   level_pop();
-  is_falling        = false;
-  is_changing_level = false;
-  is_hidden         = true;
+  is_falling                                   = false;
+  is_changing_level                            = false;
+  is_hidden                                    = true;
+  is_waiting_to_leave_level_has_completed_fall = false;
   update_interpolated_position();
   level_push();
 
