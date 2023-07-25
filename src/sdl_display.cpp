2a30e1d98ddf941d00b2d45e5e213f26cfd797d8
Neil McGill <goblinhack@gmail.com>
2023-07-25 19:42:13 +0100

    bug: console was not resized on changing gfx mode

--- src/sdl_display.cpp
+++ src/sdl_display.cpp
@@ -13,6 +13,7 @@
 #include "my_wid_leftbar.hpp"
 #include "my_wid_rightbar.hpp"
 #include "my_wid_thing_info.hpp"
+#include "my_wid_topcon.hpp"
 #include "my_wid_tp_info.hpp"
 
 void sdl_display(void)
@@ -72,11 +73,15 @@ void sdl_display_reset(void)
 {
   CON("SDL: Video resetting");
 
+  auto old_topcon  = wid_topcon_serialize();
+  auto old_console = wid_console_serialize();
+
   wid_console_fini();
   wid_rightbar_fini();
   wid_leftbar_fini();
   wid_actionbar_fini();
   wid_botcon_fini();
+  wid_topcon_fini();
   wid_asciimap_fini();
   wid_thing_info_fini("gfx toggle"); // To remove bag or other info
   wid_tp_info_fini("gfx toggle");    // To remove bag or other info
@@ -97,10 +102,13 @@ void sdl_display_reset(void)
   wid_hide(wid_console_window);
   sdl_flush_display();
 
-  wid_botcon_fini();
-  wid_botcon_init();
-  wid_visible(wid_botcon_window);
   wid_asciimap_init();
+  wid_botcon_init();
+  wid_topcon_init();
+  wid_visible(wid_topcon_window);
+  wid_visible(wid_botcon_window);
+  wid_topcon_deserialize(old_topcon);
+  wid_console_deserialize(old_console);
 
   //
   // DO NOT CALL THIS HERE. We could be inside a widget mouse up handler.
