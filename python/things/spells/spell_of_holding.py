797a9108287d3c442d958270c6cac9e20299efd1
Neil McGill <goblinhack@gmail.com>
2023-08-26 21:29:31 +0100

    bug: skill tree was too wide in ascii mode

--- python/things/spells/spell_of_holding.py
+++ python/things/spells/spell_of_holding.py
@@ -22,7 +22,7 @@ def tp_init(name, text_long_name, text_short_name):
     my.range_max(self, 7)
     my.gfx_targeted_projectile(self, "projectile_magical_effect")
     my.spell_base_name(self, name)
-    my.spell_cost(self, 2)
+    my.spell_cost(self, 3)
     my.text_a_or_an(self, "a")
     my.text_description_long(self, "Use this spell to place a magical barrier around a monster to prevent either their escape, or them attacking you!")
     my.text_description_short(self, "Spell of holding.")
