7b26f87abecb8db25ef8061f35b2cb5e159024e5
Neil McGill <goblinhack@gmail.com>
2023-08-30 19:29:26 +0100

    add other damage types

--- src/thing_template.cpp
+++ src/thing_template.cpp
@@ -442,19 +442,10 @@ void tp_dump_monsters(void)
   // | git status   | git status     | git status    |
   // | git diff     | git diff       | git diff      |
 
-  printf(
-      "DUMP: | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | "
-      "%s | %s | %s |\n",
-      "Monster", "Danger level", "Dmg:Nat Attack", "Dmg:Nat Attack Type", "Dmg:Acid", "Dmg:Bite", "Dmg:Claw",
-      "Dmg:Cold", "Dmg:Crush", "Dmg:Digest", "Dmg:Drown", "Dmg:Energy", "Dmg:Fire", "Dmg:Heat", "Dmg:Lightning",
-      "Dmg:Magic", "Dmg:Melee", "Dmg:Missile", "Dmg:Necrosis", "Dmg:Negation", "Dmg:Poison", "Dmg:Stamina",
-      "Dmg:Water");
+  printf("DUMP: | %s | %s | %s | %s | %s |\n", "Monster", "Danger level", "Dmg:Nat Attack", "Dmg:Nat Attack Type",
+         "Dmg:Other");
 
-  printf(
-      "DUMP: | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | "
-      "%s | %s | %s |\n",
-      ":---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---", "---",
-      "---", "---", "---", "---", "---", "---", "---");
+  printf("DUMP: | %s | %s | %s | %s | %s |\n", ":---", "---", "---", "---", "---");
 
   std::vector< Tpp > m;
 
@@ -467,16 +458,85 @@ void tp_dump_monsters(void)
   sort(m.begin(), m.end(), [](Tpp a, Tpp b) -> bool { return a->get_danger_level() > b->get_danger_level(); });
 
   for (auto tp : m) {
-    printf(
-        "DUMP: | %s | %u | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | "
-        "%s | %s | %s |\n",
-        capitalise(tp->text_long_name()).c_str(), tp->get_danger_level(), tp->dmg_nat_att_dice_str().c_str(),
-        tp->dmg_nat_att_type().c_str(), tp->dmg_acid_dice_str().c_str(), tp->dmg_bite_dice_str().c_str(),
-        tp->dmg_claw_dice_str().c_str(), tp->dmg_cold_dice_str().c_str(), tp->dmg_crush_dice_str().c_str(),
-        tp->dmg_digest_dice_str().c_str(), tp->dmg_drown_dice_str().c_str(), tp->dmg_energy_dice_str().c_str(),
-        tp->dmg_fire_dice_str().c_str(), tp->dmg_heat_dice_str().c_str(), tp->dmg_lightning_dice_str().c_str(),
-        tp->dmg_magic_dice_str().c_str(), tp->dmg_melee_dice_str().c_str(), tp->dmg_missile_dice_str().c_str(),
-        tp->dmg_necrosis_dice_str().c_str(), tp->dmg_negation_dice_str().c_str(), tp->dmg_poison_dice_str().c_str(),
-        tp->dmg_stamina_dice_str().c_str(), tp->dmg_water_dice_str().c_str());
+    std::string dmg_str;
+
+    if (! tp->dmg_acid_dice_str().empty()) {
+      dmg_str += " Acid:";
+      dmg_str += tp->dmg_acid_dice_str();
+    }
+    if (! tp->dmg_bite_dice_str().empty()) {
+      dmg_str += " Bite:";
+      dmg_str += tp->dmg_bite_dice_str();
+    }
+    if (! tp->dmg_claw_dice_str().empty()) {
+      dmg_str += " Claw:";
+      dmg_str += tp->dmg_claw_dice_str();
+    }
+    if (! tp->dmg_cold_dice_str().empty()) {
+      dmg_str += " Cold:";
+      dmg_str += tp->dmg_cold_dice_str();
+    }
+    if (! tp->dmg_crush_dice_str().empty()) {
+      dmg_str += "Crush:";
+      dmg_str += tp->dmg_crush_dice_str();
+    }
+    if (! tp->dmg_digest_dice_str().empty()) {
+      dmg_str += "Digest:";
+      dmg_str += tp->dmg_digest_dice_str();
+    }
+    if (! tp->dmg_drown_dice_str().empty()) {
+      dmg_str += "Drown:";
+      dmg_str += tp->dmg_drown_dice_str();
+    }
+    if (! tp->dmg_energy_dice_str().empty()) {
+      dmg_str += "Energy:";
+      dmg_str += tp->dmg_energy_dice_str();
+    }
+    if (! tp->dmg_fire_dice_str().empty()) {
+      dmg_str += "Fire:";
+      dmg_str += tp->dmg_fire_dice_str();
+    }
+    if (! tp->dmg_heat_dice_str().empty()) {
+      dmg_str += "Heat:";
+      dmg_str += tp->dmg_heat_dice_str();
+    }
+    if (! tp->dmg_lightning_dice_str().empty()) {
+      dmg_str += "Lightning:";
+      dmg_str += tp->dmg_lightning_dice_str();
+    }
+    if (! tp->dmg_magic_dice_str().empty()) {
+      dmg_str += "Magic:";
+      dmg_str += tp->dmg_magic_dice_str();
+    }
+    if (! tp->dmg_melee_dice_str().empty()) {
+      dmg_str += "Melee:";
+      dmg_str += tp->dmg_melee_dice_str();
+    }
+    if (! tp->dmg_missile_dice_str().empty()) {
+      dmg_str += "Missile:";
+      dmg_str += tp->dmg_missile_dice_str();
+    }
+    if (! tp->dmg_necrosis_dice_str().empty()) {
+      dmg_str += "Necrosis:";
+      dmg_str += tp->dmg_necrosis_dice_str();
+    }
+    if (! tp->dmg_negation_dice_str().empty()) {
+      dmg_str += "Negation:";
+      dmg_str += tp->dmg_negation_dice_str();
+    }
+    if (! tp->dmg_poison_dice_str().empty()) {
+      dmg_str += "Poison:";
+      dmg_str += tp->dmg_poison_dice_str();
+    }
+    if (! tp->dmg_stamina_dice_str().empty()) {
+      dmg_str += "Stamina:";
+      dmg_str += tp->dmg_stamina_dice_str();
+    }
+    if (! tp->dmg_water_dice_str().empty()) {
+      dmg_str += "Water:";
+      dmg_str += tp->dmg_water_dice_str();
+    }
+    printf("DUMP: | %s | %u | %s | %s | %s |\n", capitalise(tp->text_long_name()).c_str(), tp->get_danger_level(),
+           tp->dmg_nat_att_dice_str().c_str(), tp->dmg_nat_att_type().c_str(), dmg_str.c_str());
   }
 }
