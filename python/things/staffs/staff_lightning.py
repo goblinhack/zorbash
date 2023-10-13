29b2e3e284e884f5bba422f190c80d2272ec18f3
Neil McGill <goblinhack@gmail.com>
2023-10-13 18:22:51 +0100

    bug: staff of lightning seemed underpowered

--- python/things/staffs/staff_lightning.py
+++ python/things/staffs/staff_lightning.py
@@ -65,7 +65,7 @@ def tp_init(name, text_long_name, text_short_name):
     my.charge_count(self, 3)
     my.collision_hit_priority(self, 6)
     my.dmg_chance_d1000_lightning(self, 0, 1000)  # for electric eel attack
-    my.dmg_lightning_dice(self, "1d10+4")
+    my.dmg_lightning_dice(self, "1d20+4")
     my.equip_carry_anim(self, "staff_lightning_carry")
     my.gfx_ascii_fade_with_dist(self, True)
     my.gfx_ascii_shown(self, True)
