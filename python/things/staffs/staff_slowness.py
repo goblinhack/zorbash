c8a9e474305556a096f58f6b7551f375a97992e8
Neil McGill <goblinhack@gmail.com>
2023-10-27 14:00:43 +0100

    bug: fix staff of slowness carrying error

--- python/things/staffs/staff_slowness.py
+++ python/things/staffs/staff_slowness.py
@@ -90,7 +90,7 @@ def tp_init(name, text_long_name, text_short_name):
     my.collision_hit_priority(self, 6)
     my.dmg_num_of_attacks(self, 0)
     my.effect_has_blast_radius(self, True)
-    my.equip_carry_anim(self, "staff_slow_carry")
+    my.equip_carry_anim(self, "staff_slowness_carry")
     my.gfx_ascii_fade_with_dist(self, True)
     my.gfx_ascii_shown(self, True)
     my.gfx_pixelart_animated(self, True)
