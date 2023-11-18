a955be44d38fe955ab2d151bdf9a39b9961782f3
Neil McGill <goblinhack@gmail.com>
2023-11-18 16:35:19 +0000

    feat: better dogman gfx

--- python/things/monsters/dogman.py
+++ python/things/monsters/dogman.py
@@ -203,12 +203,6 @@ def tp_init(name, text_long_name):
     my.tile(self,
             ascii_fg_char="d", ascii_bg_col_name="", ascii_fg_col_name="gray30",
             tile="dogman.dead.1", is_dead=True, delay_ms=delay)
-    my.tile(self,
-            ascii_fg_char="d", ascii_bg_col_name="", ascii_fg_col_name="gray30",
-            tile="dogman.dead.2", is_dead=True, delay_ms=delay)
-    my.tile(self,
-            ascii_fg_char="d", ascii_bg_col_name="", ascii_fg_col_name="gray30",
-            tile="dogman.dead.3", is_dead=True, delay_ms=delay, is_end_of_anim=True)
 
     my.tp_update(self)
 
