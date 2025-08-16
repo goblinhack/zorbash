92c3e5ae342a01ab5c4a82ca4b7b52cc9d968ce4
Goblin Hack <goblinhack@gmail.com>
2025-08-16 20:20:48 +0100

    dirt was too dirty and hard to see

--- python/things/dungeon/dirt.py
+++ python/things/dungeon/dirt.py
@@ -34,7 +34,7 @@ def tp_init(name, tiles=[], left1_tiles=[], bot3_tiles=[]):
 
     for t in tiles:
         my.tile(self,
-                ascii_fg_char="middle_dot", ascii_fg_col_name="brown",
+                ascii_fg_char="middle_dot", ascii_fg_col_name="saddlebrown",
                 tile=t)
 
     if bot3_tiles is not None:
